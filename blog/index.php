65e669dc3da9d496b8f12d3493b0dec4f5fa56d1
sven-berger <bergersvenoliver@gmail.com>
2024-10-18 11:26:08 +0200

    CV-1 Beginn des Projektes

---
+++ blog/index.php
@@ -0,0 +1,13 @@
+<?php
+    $bereich = 'Blog';
+    $pageTitle = 'Blog von CodeVoyage.de';
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/header/index.header.inc.php");
+?>
+
+<?php
+    echo "Hallo und herzlich willkommen im Blog von CodeVoyage.de"
+?>
+
+<?php
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/footer/index.footer.inc.php");
+?>
