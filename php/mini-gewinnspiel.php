d421fcddcda4b0c1669172a4c06f8d409b871d5a
sven-berger <bergersvenoliver@gmail.com>
2024-10-22 16:12:31 +0200

    main: Erstveröffentlichung

---
+++ php/mini-gewinnspiel.php
@@ -0,0 +1,29 @@
+<?php
+    $bereich = 'PHP-Bereich';
+    $pageTitle = "Mini-Gewinnspiel";
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/header/php.header.inc.php");
+?>
+
+<form action="mini-gewinnspiel.php" method="get">
+    <label for="zahl">Bitte gib eine Zahl zwischen 1 und 10 ein:</label>
+        <input type="number" id="zahl" name="zahl" min="1" max="10" required>
+    <button type="submit">Absenden</button>
+</form>
+
+<?php if (isset($_GET['zahl'])): ?>
+    <?php while ($_GET['zahl']) != 7: ?>
+        <p>Leider falsch, nächster Versuch!</p>
+        <form action="mini-gewinnspiel.php" method="get">
+            <label for="zahl">Bitte gib eine Zahl zwischen 1 und 10 ein:</label>
+                <input type="number" id="zahl" name="zahl" min="1" max="10" required>
+            <button type="submit">Absenden</button>
+        </form>
+    <?php else: ?>
+        <p>Herzlichen Glückwunsch, du hast gewonnen!</p>
+    <?php endwhile; ?>
+<?php endif; ?>
+
+
+<?php
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/footer/php.footer.inc.php");
+?>
