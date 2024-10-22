9ed361e653fb3a0f0524a35ea38f3922a45ec6be
sven-berger <bergersvenoliver@gmail.com>
2024-10-22 18:51:52 +0200

    main: new version

--- php/mini-gewinnspiel.php
+++ php/mini-gewinnspiel.php
@@ -4,25 +4,28 @@
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/header/php.header.inc.php");
 ?>
 
-<?php if (isset($_GET['zahl'])): ?>
-<?php $zahl = (int)$_GET['zahl']; ?>
-    <?php if ($zahl != 7): ?>
-        <p>Leider falsch, nächster Versuch!</p>
-        <form action="mini-gewinnspiel.php" method="get">
-            <label for="zahl">Bitte gib eine Zahl zwischen 1 und 10 ein:</label>
-            <input type="number" id="zahl" name="zahl" min="1" max="10" required>
-            <button type="submit">Absenden</button>
-        </form>
-    <?php else: ?>
-        <p>Herzlichen Glückwunsch, du hast gewonnen!</p>
+<?php
+    $eingabe = null;
+?>
+
+<?php while ($eingabe !== 7): ?>
+    <?php if (isset($_GET['eingabe'])): ?>
+        <?php
+            $eingabe = (int)$_GET['eingabe'];
+            if ($eingabe === 7) {
+                echo "<p>Herzlichen Glückwunsch, du hast richtig geraten!</p>";
+                break;
+            } else {
+                echo "<p>Leider falsch, versuche es erneut.</p>";
+            }
+        ?>
     <?php endif; ?>
-<?php else: ?>
     <form action="mini-gewinnspiel.php" method="get">
-        <label for="zahl">Bitte gib eine Zahl zwischen 1 und 10 ein:</label>
-        <input type="number" id="zahl" name="zahl" min="1" max="10" required>
+        <label for="eingabe">Bitte gib eine Eingabe zwischen 1 und 10 ein:</label>
+        <input type="number" id="eingabe" name="eingabe" min="1" max="10" required>
         <button type="submit">Absenden</button>
     </form>
-<?php endif; ?>
+<?php endwhile; ?>
 
 <?php
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/footer/php.footer.inc.php");
