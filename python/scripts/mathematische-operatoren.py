1e9eb9dcf7574a730dbb1bd3fe9ff97cc1c340b2
sven-berger <bergersvenoliver@gmail.com>
2024-10-22 20:13:03 +0200

    main: add new mini-apps

---
+++ python/scripts/mathematische-operatoren.py
@@ -0,0 +1,19 @@
+zahl = int(input("Bitte gib eine Zahl ein: "))
+vergleich = int(input("Bitte gib eine Zahl ein, mit der du deine Zahl vergleichen möchtest: "))
+
+if zahl < vergleich:
+    print("Deine Zahl ist kleiner.")
+elif zahl > vergleich:
+    print("Deine Zahl ist größer.")
+else:
+    print("Du hast die selbe Zahl eingegeben.")
+    
+zahl2 = int(input("Bitte gib eine zweite Zahl ein: "))
+vergleich2 = int(input("Bitte gib eine zweite Zahl ein, mit der du deine (zweite) Zahl vergleichen möchtest: "))
+
+if zahl <= vergleich:
+    print("Deine Zahl ist kleiner oder gleich.")
+elif zahl >= vergleich:
+    print("Deine Zahl ist größer oder gleich.")
+else:
+    print("Du hast die selbe Zahl eingegeben.")
