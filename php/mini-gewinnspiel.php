36542be5414efddc0f4869300b73b597ae49e28a
sven-berger <bergersvenoliver@gmail.com>
2024-10-22 18:53:31 +0200

    Bugfix

--- php/mini-gewinnspiel.php
+++ php/mini-gewinnspiel.php
@@ -8,24 +8,24 @@
     $eingabe = null;
 ?>
 
+<?php if (isset($_GET['eingabe'])): ?>
 <?php while ($eingabe !== 7): ?>
-    <?php if (isset($_GET['eingabe'])): ?>
-        <?php
-            $eingabe = (int)$_GET['eingabe'];
-            if ($eingabe === 7) {
-                echo "<p>Herzlichen Glückwunsch, du hast richtig geraten!</p>";
-                break;
-            } else {
-                echo "<p>Leider falsch, versuche es erneut.</p>";
-            }
-        ?>
-    <?php endif; ?>
+    <?php
+        $eingabe = (int)$_GET['eingabe'];
+        if ($eingabe === 7) {
+            echo "<p>Herzlichen Glückwunsch, du hast richtig geraten!</p>";
+            break;
+        } else {
+            echo "<p>Leider falsch, versuche es erneut.</p>";
+        }
+    ?>
     <form action="mini-gewinnspiel.php" method="get">
         <label for="eingabe">Bitte gib eine Eingabe zwischen 1 und 10 ein:</label>
         <input type="number" id="eingabe" name="eingabe" min="1" max="10" required>
         <button type="submit">Absenden</button>
     </form>
 <?php endwhile; ?>
+<?php endif; ?>
 
 <?php
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/footer/php.footer.inc.php");
