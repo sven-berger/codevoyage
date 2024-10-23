22d195b3d21ab8169b958dd65d3d3d15c3d81a0f
sven-berger <bergersvenoliver@gmail.com>
2024-10-23 10:40:45 +0200

    Update umsatzrechner.py

--- python/scripts/umsatzrechner.py
+++ python/scripts/umsatzrechner.py
@@ -1,16 +1,16 @@
-januar = int(input("Bitte gib den Umsatz für Januar an: "))
-februar = int(input("Bitte gib den Umsatz für Februar an: "))
-maerz = int(input("Bitte gib den Umsatz für März an: "))
-april = int(input("Bitte gib den Umsatz für April an: "))
-mai = int(input("Bitte gib den Umsatz für Mai an: "))
-juni = int(input("Bitte gib den Umsatz für Juni an: "))
-juli = int(input("Bitte gib den Umsatz für Juli an: "))
-august = int(input("Bitte gib den Umsatz für August an: "))
-september = int(input("Bitte gib den Umsatz für September an: "))
-oktober = int(input("Bitte gib den Umsatz für Oktober an: "))
-november = int(input("Bitte gib den Umsatz für November an: "))
-dezember = int(input("Bitte gib den Umsatz für Dezember an: "))
-guter_monat = int(input("Bitte gib zum Schluss noch an, was für dich ein guter Monat ist: "))
+januar = float(input("Bitte gib den Umsatz für Januar an: "))
+februar = float(input("Bitte gib den Umsatz für Februar an: "))
+maerz = float(input("Bitte gib den Umsatz für März an: "))
+april = float(input("Bitte gib den Umsatz für April an: "))
+mai = float(input("Bitte gib den Umsatz für Mai an: "))
+juni = float(input("Bitte gib den Umsatz für Juni an: "))
+juli = float(input("Bitte gib den Umsatz für Juli an: "))
+august = float(input("Bitte gib den Umsatz für August an: "))
+september = float(input("Bitte gib den Umsatz für September an: "))
+oktober = float(input("Bitte gib den Umsatz für Oktober an: "))
+november = float(input("Bitte gib den Umsatz für November an: "))
+dezember = float(input("Bitte gib den Umsatz für Dezember an: "))
+guter_monat = float(input("Bitte gib zum Schluss noch an, was für dich ein guter Monat ist: "))
 
 monatlicher_umsatz = [januar, februar, maerz, april, mai, juni, juli, august, september, oktober, november, dezember]
 gesamtumsatz = sum(monatlicher_umsatz)
@@ -20,9 +20,9 @@ umsatz_gute_monate = 0
 
 for umsatz in monatlicher_umsatz:
     if umsatz >= guter_monat:
-        umsatz_gute_monate += umsatz
         anzahl_gute_monate += 1
+        umsatz_gute_monate += umsatz
 
 print("Der Gesamtumsatz beträgt: " + str(gesamtumsatz) + "€")
-print("Anzahl der guten Monate: " + str(anzahl_gute_monate))
+print("Anzahl der guten Monate: " + str(anzahl_gute_monate) + " Monate")
 print("Umsatz der guten Monate: " + str(umsatz_gute_monate) + "€")
