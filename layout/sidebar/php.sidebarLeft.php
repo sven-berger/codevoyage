dddcffbe4e41a32c21c0a21a26999eae98d926b9
sven-berger <bergersvenoliver@gmail.com>
2024-10-23 11:14:13 +0200

    Update php.sidebarLeft.php

--- layout/sidebar/php.sidebarLeft.php
+++ layout/sidebar/php.sidebarLeft.php
@@ -10,9 +10,6 @@
                 <li><a href="https://php.codevoyage.de/passwortgenerator.php">Passwortgenerator</a></li>
                 <li><a href="https://php.codevoyage.de/einfaches-gewinnspiel.php">Einfaches Gewinnspiel</a></li>
                 <li><a href="https://php.codevoyage.de/mini-taschenrechner.php">Mini-Taschenrechner</a></li>
-                <li><a href="https://php.codevoyage.de/mathematische-operatoren-teil1.php">Mathematische Operatoren: Teil 1</a></li>
-                <li><a href="https://php.codevoyage.de/mathematische-operatoren-teil2.php">Mathematische Operatoren: Teil 2</a></li>
-                <li><a href="https://php.codevoyage.de/mathematische-operatoren-teil3.php">Mathematische Operatoren: Teil 3</a></li>
             </ul>
         </div>
     </section>
@@ -25,10 +22,27 @@
             <ul>
                 <li><a href="https://php.codevoyage.de/screensaver.php">Screensaver</a></li>
                 <li><a href="https://php.codevoyage.de/schwarzes_loch.php">Ein schwarzes Loch</a></li>
-                <li><a href="https://php.codevoyage.de/anleitung-github.php">Eine Kurzanleitung für GitHub</a></li>
-                <li><a href="https://php.codevoyage.de/anleitung-flask.php">Eine Kurzanleitung für Flask</a></li>
-                <li><a href="https://php.codevoyage.de/database-test.php">Datenbanktest</a></li>
             </ul>
         </div>
     </section>
+
+    <div class="boxCapital">
+        <p>Sonstiges</p>
+    </div>
+    <section class="sidebarBox">
+        <div class="boxContent">
+            <ul>
+                <li><a href="https://php.codevoyage.de/mathematische-operatoren-teil1.php">Mathematische Operatoren: Teil 1</a></li>
+                <li><a href="https://php.codevoyage.de/mathematische-operatoren-teil2.php">Mathematische Operatoren: Teil 2</a></li>
+                <li><a href="https://php.codevoyage.de/mathematische-operatoren-teil3.php">Mathematische Operatoren: Teil 3</a></li>
+            </ul><br/>
+            <ul>
+                <li><a href="https://php.codevoyage.de/anleitung-github.php">Eine Kurzanleitung für GitHub</a></li>
+                <li><a href="https://php.codevoyage.de/anleitung-flask.php">Eine Kurzanleitung für Flask</a></li>
+            </ul><br/>
+            <ul>
+                <li><a href="https://php.codevoyage.de/database-test.php">Datenbanktest</a></li>
+            </ul>
+        </div>
+    </section>    
 </nav>
