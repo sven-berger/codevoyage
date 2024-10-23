08b6ae462e7ffcca24b2c76b822a35f31acabe8a
sven-berger <bergersvenoliver@gmail.com>
2024-10-23 09:36:17 +0200

    Update mathematische-operatoren-teil1.php

--- php/mathematische-operatoren-teil1.php
+++ php/mathematische-operatoren-teil1.php
@@ -14,24 +14,29 @@
     <button type="submit">Eingabe abschicken</button>
 </form>
 
-<?php
-if (isset($_GET['zahl1']) && isset($_GET['vergleich_zahl1'])) {
-    $erste_zahl = (float)$_GET['zahl1'];
-    $vergleich_zahl1 = (float)$_GET['vergleich_zahl1'];
-    
-    try {
-        if ($erste_zahl < $vergleich_zahl1) {
-            echo "$erste_zahl ist <strong>kleiner</strong> als $vergleich_zahl1.";
-        } elseif ($erste_zahl > $vergleich_zahl1) {
-            echo "$erste_zahl ist <strong>größer</strong> als $vergleich_zahl1.";
-        } else {
-            echo "Du hast die gleiche Zahl eingegeben.";
-        }
-    } catch (Exception $e) {
-        echo "Fehler: " . $e->getMessage();
-    }
-}
-?>
+<?php if (isset($_GET['zahl1']) && isset($_GET['vergleich_zahl1'])): ?>
+    </div>
+    </section>
+    <?php
+        $erste_zahl = (float)$_GET['zahl1'];
+        $vergleich_zahl1 = (float)$_GET['vergleich_zahl1'];
+    ?>
+    <?php if ($erste_zahl < $vergleich_zahl1): ?>
+        <section class="section">
+            <div class="sectionContent">
+                <?php echo $erste_zahl; ?> ist <strong>kleiner</strong> als <?php echo $vergleich_zahl1; ?>.
+        <?php elseif ($erste_zahl > $vergleich_zahl1): ?>
+             <section class="section">
+            <div class="sectionContent">
+            <?php echo $erste_zahl; ?> ist <strong>größer</strong> als <?php echo $vergleich_zahl1; ?>.
+
+        <?php else: ?>
+            <section class="section">
+            <div class="sectionContent">
+                <p>Du hast die gleiche Zahl eingegeben.</p>
+        <?php endif; ?>
+
+<?php endif; ?>
 
 <?php
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/footer/php.footer.inc.php");
