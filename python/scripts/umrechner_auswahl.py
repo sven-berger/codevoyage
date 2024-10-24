577726a2c3b6cca9eca65bbc492ddca8d1810f05
sven-berger <bergersvenoliver@gmail.com>
2024-10-24 15:25:07 +0200

    new

---
+++ python/scripts/umrechner_auswahl.py
@@ -0,0 +1,24 @@
+umrechnung = {
+    "B": 1,
+    
+    "GiB": 1024**3,
+    "MiB": 1024**2,
+    "KiB": 1024,
+    
+    "GB": 1000**3,
+    "MB": 1000**2,
+    "KB": 1000,
+    
+    "Bit": 1 / 8
+}
+
+zahl = float(input("Bitte gib die Zahl ein, die du umrechnen möchtest: "))
+ausgang = input("Von welcher Einheit möchtest du umrechnen? (GiB, MiB, KiB, GB, MB, KB, B, Bit): ")
+ziel = input("In welche Einheit möchtest du umrechnen? (GiB, MiB, KiB, GB, MB, KB, B, Bit): ")
+    
+if ausgang in umrechnung and ziel in umrechnung:
+    zahl_in_bytes = zahl * umrechnung[ausgang]
+    ergebnis = zahl_in_bytes / umrechnung[ziel]
+    print(f"{zahl} {ausgang} entsprechen {ergebnis} {ziel}.")
+else:
+    print("Ungültige Einheit! Bitte gib GiB, MiB, KiB, GB, MB, KB, B, Bit ein.")
