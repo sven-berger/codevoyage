30ddd3de4eb3acb47539e6835cf3709d82f7aef9
sven-berger <bergersvenoliver@gmail.com>
2024-10-24 15:50:26 +0200

    Update

--- python/scripts/speicherdaten-umrechner.py
+++ python/scripts/speicherdaten-umrechner.py
@@ -1,5 +1,5 @@
 umrechnung = {
-    "B": 1,
+    "Byte": 1,
     
     "GiB": 1024**3,
     "MiB": 1024**2,
@@ -13,8 +13,8 @@ umrechnung = {
 }
 
 zahl = float(input("Bitte gib die Zahl ein, die du umrechnen möchtest: "))
-ausgang = input("Von welcher Einheit möchtest du umrechnen? (GiB, MiB, KiB, GB, MB, KB, B, Bit): ")
-ziel = input("In welche Einheit möchtest du umrechnen? (GiB, MiB, KiB, GB, MB, KB, B, Bit): ")
+ausgang = input("Von welcher Einheit möchtest du umrechnen? (GiB, MiB, KiB, GB, MB, KB, Byte, Bit): ")
+ziel = input("In welche Einheit möchtest du umrechnen? (GiB, MiB, KiB, GB, MB, KB, Byte, Bit): ")
     
 if ausgang in umrechnung and ziel in umrechnung:
     zahl_in_bytes = zahl * umrechnung[ausgang]
