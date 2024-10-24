f32e6f4877b49aca230bd939d5dedd1e08b77f28
sven-berger <bergersvenoliver@gmail.com>
2024-10-24 09:38:19 +0200

    Update speicherdaten-umrechner.php

--- php/speicherdaten-umrechner.php
+++ php/speicherdaten-umrechner.php
@@ -62,9 +62,18 @@
     <section class="section">
         <div class="sectionContent">
             <div class="sectionHeader">Das Ergebnis der Umrechnung ist:</div>
-                <strong><?php echo $vorhandene_einheit . $vorhandener_praefix ?></strong> entsprechen: <strong><?php echo $ergebnis . $gesuchter_praefix; ?></strong>
+                <strong><?php echo $vorhandene_einheit . " " . $vorhandener_praefix ?></strong> entsprechen: <strong><?php echo $ergebnis . " " . $gesuchter_praefix; ?></strong>
 <?php endif; ?>
 
 <?php
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/footer/php.footer.inc.php");
 ?>
+
+
+<style>
+
+.form-speicher label {
+    margin-top: 10px;
+}
+
+</style>
