1f5ea183651b639a0085790dca65bf382fcf6fad
sven-berger <bergersvenoliver@gmail.com>
2024-10-25 13:05:56 +0200

    ets

--- blog/index.php
+++ blog/index.php
@@ -1,20 +1,33 @@
 <?php
     $bereich = 'Blog';
-    $pageTitle = 'Blog von CodeVoyage.de';
-    require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/header/index.header.inc.php");
+    $pageTitle = "Blog";
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/includes/header.inc.php");
 ?>
 
-<?php
+<?php 
+
 try {
     $sql = "SELECT * FROM `blog`";
     $result = $connection->query($sql);
-    $ausgabe = $result->fetchAll(PDO::FETCH_ASSOC);
+    $rows = $result->fetchAll(PDO::FETCH_ASSOC);
+?>
+    <?php if ($result->rowCount() > 0): ?>
+        <?php foreach ($rows as $row): ?>
+            <h2><span><?php echo htmlspecialchars($row['id']); ?> | </span><?php echo htmlspecialchars($row['ueberschrift']); ?></h2>
+            <h4><?php echo htmlspecialchars($row['kurzbeschreibung']); ?></h4>
+            <p><?php echo $row['inhalt']; ?></p>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <p style="text-align: center;">Keine Blog-Artikel gefunden.</p>
+    <?php endif; ?>
+<?php 
 } catch (PDOException $e) {
-    echo '<p style="text-align: center;">Es liegt ein Problem vor: ' . $e->getMessage() . '</p>';
+    echo '<p style="text-align: center;">Es liegt ein Problem vor: ' . htmlspecialchars($e->getMessage()) . '</p>';
 }
+
 ?>
 
 
 <?php
-    require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/footer/index.footer.inc.php");
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/includes/footer.inc.php");
 ?>
