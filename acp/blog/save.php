8b5a908209c68d222c99bdfa8cf583c51254cadf
sven-berger <bergersvenoliver@gmail.com>
2024-10-25 12:38:41 +0200

    Update save.php

--- acp/blog/save.php
+++ acp/blog/save.php
@@ -2,28 +2,28 @@
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     try {
-        if (isset($_POST['ueberschrift']) && !empty($_POST['kurzbeschreibung']) && !empty($_POST['inhalt'])) {
+        if (!empty($_POST['ueberschrift']) && !empty($_POST['kurzbeschreibung']) && !empty($_POST['inhalt'])) {
+            // Eingaben sanitizen
             $ueberschrift = filter_input(INPUT_POST, 'ueberschrift', FILTER_SANITIZE_SPECIAL_CHARS);
             $kurzbeschreibung = filter_input(INPUT_POST, 'kurzbeschreibung', FILTER_SANITIZE_SPECIAL_CHARS);
             $inhalt = filter_input(INPUT_POST, 'inhalt', FILTER_SANITIZE_SPECIAL_CHARS);
 
+            // SQL-Anfrage vorbereiten und ausführen
             $prepare = $connection->prepare('INSERT INTO `blog` (`ueberschrift`, `kurzbeschreibung`, `inhalt`) VALUES (:ueberschrift, :kurzbeschreibung, :inhalt)');
             $prepare->bindParam(':ueberschrift', $ueberschrift, PDO::PARAM_STR);
             $prepare->bindParam(':kurzbeschreibung', $kurzbeschreibung, PDO::PARAM_STR);
             $prepare->bindParam(':inhalt', $inhalt, PDO::PARAM_STR);
             $prepare->execute();
 
+            // Erfolgsmeldung und Weiterleitung
             echo 'Blog-Artikel erfolgreich veröffentlicht.';
             header("Location: https://codevoyage.de/acp/blog/index.php");
-             exit();
+            exit();
         } else {
             echo 'Bitte füllen Sie alle Felder aus.';
         }
     } catch (PDOException $e) {
-        echo 'Es liegt ein Problem vor: ' . $e->getMessage();
-        echo "<pre>";
-        var_dump($e->getMessage());
-        echo "</pre>";
+        echo 'Es liegt ein Problem vor: ' . htmlspecialchars($e->getMessage());
     }
 }
 
