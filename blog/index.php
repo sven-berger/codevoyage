41ddbb895639c227efe36a5e4cb83543df42d16f
sven-berger <bergersvenoliver@gmail.com>
2024-10-25 13:39:53 +0200

    Update index.php

--- blog/index.php
+++ blog/index.php
@@ -12,15 +12,20 @@ try {
     $rows = $result->fetchAll(PDO::FETCH_ASSOC);
 ?>
     <?php if ($result->rowCount() > 0): ?>
-        <p>dsafs</p>
-        </div>
-        </section>
-        <section class="section">
-            <div class="sectionContent">
+        
+        
+        
         <?php foreach ($rows as $row): ?>
+            <section class="section">
+            <div class="sectionContent">
             <h2><?php echo htmlspecialchars($row['ueberschrift']); ?></h2>
             <h4><?php echo htmlspecialchars($row['kurzbeschreibung']); ?></h4>
             <p><?php echo $row['inhalt']; ?></p>
+            </div>
+            </section>
+            <section class="section">
+            <div class="sectionContent">
+            <p>dsafs</p>
         <?php endforeach; ?>
     <?php else: ?>
         <p style="text-align: center;">Keine Blog-Artikel gefunden.</p>
