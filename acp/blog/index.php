02b9e03e42f19deca32a200ad189d52c2958743c
sven-berger <bergersvenoliver@gmail.com>
2024-10-25 12:53:11 +0200

    Update index.php

--- acp/blog/index.php
+++ acp/blog/index.php
@@ -4,7 +4,39 @@
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/acp/includes/header.inc.php");
 ?>
 
-<p>Hallo</p>
+<?php
+
+try {
+    $sql = "SELECT * FROM `blog`";
+    $result = $connection->query($sql);
+
+    if ($result->rowCount() > 0) {
+        echo "<table>";
+        echo "<tr><th>Monat</th><th>Umsatz</th><th>Aktion</th></tr>";
+
+        $rows = $result->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($rows as $row) {
+            echo "<tr>";
+            $monats_name = $monate_zuweisung[$row['ueberschrift']];
+            
+            echo "<td>" . htmlspecialchars($kurzbeschreibung) . "</td>";
+            echo "<td>" . htmlspecialchars($row['inhalt']) . "€</td>";
+            echo "<td>
+                    <a href='edit.php?id=" . htmlspecialchars($row['ID']) . "'>Bearbeiten</a> |
+                    <a href='delete.php?id=" . htmlspecialchars($row['ID']) . "' onclick='return confirm(\"Bist du dir sicher, dass du diesen Eintrag löschen möchtest?\");'>Löschen</a>
+                  </td>";
+            echo "</tr>";
+        }
+
+        echo "</table>";
+    } else {
+        echo "<p style='text-align: center;'>Keine Umsatzzahlen gefunden.</p>";
+    }
+} catch (PDOException $e) {
+    echo '<p style="text-align: center;">Es liegt ein Problem vor: ' . $e->getMessage() . '</p>';
+}
+
+?>
 
 <?php
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/acp/includes/footer.inc.php");
