dcdb76956766be2ecf43f43256c54517258ec763
sven-berger <bergersvenoliver@gmail.com>
2024-10-25 12:57:49 +0200

    Update index.php

--- acp/blog/index.php
+++ acp/blog/index.php
@@ -4,35 +4,23 @@
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/acp/includes/header.inc.php");
 ?>
 
-<?php
+<?php 
 
 try {
     $sql = "SELECT * FROM `blog`";
     $result = $connection->query($sql);
-
-    if ($result->rowCount() > 0) {
-        echo "<table>";
-        echo "<tr><th>Monat</th><th>Umsatz</th><th>Aktion</th></tr>";
-
-        $rows = $result->fetchAll(PDO::FETCH_ASSOC);
-        foreach ($rows as $row) {
-            echo "<tr>";
-            $monats_name = $monate_zuweisung[$row['ueberschrift']];
-            
-            echo "<td>" . htmlspecialchars($kurzbeschreibung) . "</td>";
-            echo "<td>" . htmlspecialchars($row['inhalt']) . "€</td>";
-            echo "<td>
-                    <a href='edit.php?id=" . htmlspecialchars($row['ID']) . "'>Bearbeiten</a> |
-                    <a href='delete.php?id=" . htmlspecialchars($row['ID']) . "' onclick='return confirm(\"Bist du dir sicher, dass du diesen Eintrag löschen möchtest?\");'>Löschen</a>
-                  </td>";
-            echo "</tr>";
-        }
-
-        echo "</table>";
-    } else {
-        echo "<p style='text-align: center;'>Keine Umsatzzahlen gefunden.</p>";
-    }
-} catch (PDOException $e) {
+    $rows = $result->fetchAll(PDO::FETCH_ASSOC);
+?>
+   <?php if ($result->rowCount() > 0): ?>
+        <?php foreach ($rows as $row): ?>
+            <h2><?php echo htmlspecialchars($ueberschrift); ?></h2>
+            <h4><?php echo htmlspecialchars($kurzbeschreibung); ?></h4>
+            <p><?php echo htmlspecialchars($inhalt); ?>
+        <?php endforeach; ?>
+    <?php else: ?>
+        echo "<p style='text-align: center;'>Keine Blog-Artikel gefunden.</p>";
+    <?php endif; ?>
+<?php } catch (PDOException $e) {
     echo '<p style="text-align: center;">Es liegt ein Problem vor: ' . $e->getMessage() . '</p>';
 }
 
