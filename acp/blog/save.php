14c56170313fcfe6fa73a951a1a167ce7aa9b5ef
sven-berger <bergersvenoliver@gmail.com>
2024-10-25 12:35:27 +0200

    Update save.php

--- acp/blog/save.php
+++ acp/blog/save.php
@@ -2,7 +2,7 @@
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     try {
-        if (isset($_POST['ueberschrift']) && !empty($_POST['kurzbeschreibung']) && !empty($_POST['inhalt']) && !empty($_POST['blog'])) {
+        if (isset($_POST['ueberschrift']) && !empty($_POST['kurzbeschreibung']) && !empty($_POST['inhalt'])) {
             $ueberschrift = filter_input(INPUT_POST, 'ueberschrift', FILTER_SANITIZE_SPECIAL_CHARS);
             $kurzbeschreibung = filter_input(INPUT_POST, 'kurzbeschreibung', FILTER_SANITIZE_SPECIAL_CHARS);
             $inhalt = filter_input(INPUT_POST, 'inhalt', FILTER_SANITIZE_SPECIAL_CHARS);
