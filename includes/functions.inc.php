d330e3539934fab204e2d864e402090b00ed7180
sven-berger <bergersvenoliver@gmail.com>
2024-10-26 13:24:50 +0200

    Update functions.inc.php

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -5,17 +5,11 @@ try {
     $eigene_werke = "SELECT * FROM `php_sidebar_left_eigene_werke`";
     $spielereien = "SELECT * FROM `php_sidebar_left_spielereien`";
 
-    $acp_eigene_werke = "SELECT * FROM `acp_sidebar_left_eigene_werke`";
-
     $ausgabe_eigene_werke = $connection->query($eigene_werke);
     $ausgabe_spielereien = $connection->query($spielereien);
 
-    $ausgabe_acp_eigene_werke = $connection->query($acp_eigene_werke);
-
     $eigene_werke_liste = $ausgabe_eigene_werke->fetchAll(PDO::FETCH_ASSOC);
     $spielereien_liste = $ausgabe_spielereien->fetchAll(PDO::FETCH_ASSOC);
-    $acp_eigene_werke_liste = $acp_ausgabe_eigene_werke->fetchAll(PDO::FETCH_ASSOC);
-
 
 } catch (PDOException $e) {
     echo "<p style='color:red;'>Fehler bei der Abfrage: " . htmlspecialchars($e->getMessage()) . "</p>";
