543fa925f3462d0ff59fe6b00a3b0d37d16667a1
sven-berger <bergersvenoliver@gmail.com>
2024-10-27 11:08:55 +0100

    Update core.header.inc.php

--- layout/header/core.header.inc.php
+++ layout/header/core.header.inc.php
@@ -13,6 +13,7 @@
 
 <?php
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/includes/database.inc.php");
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/includes/functions.inc.php");
     $mariadbVersion = getMariaDBVersion($connection);
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/includes/var.inc.php");
 ?>
