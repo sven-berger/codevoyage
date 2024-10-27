b802c8811fece837b8a157cb1d314ad64116aab3
sven-berger <bergersvenoliver@gmail.com>
2024-10-27 10:45:17 +0100

    s<d

---
+++ acp/sidebar/left/php/sonstiges/add.php
@@ -0,0 +1,35 @@
+<?php
+    $bereich = 'Administrationsbereich';
+    $pageTitle = "Spielerei hinzufügen (PHP-Bereich)";
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/header/app.header.inc.php");
+
+$sql = "
+CREATE TABLE IF NOT EXISTS `php_sidebar_left_sonstiges`
+(
+`ID` INT NOT NULL AUTO_INCREMENT,
+`url` TEXT NOT NULL,
+`ziel` VARCHAR(255) NOT NULL,
+PRIMARY KEY (`ID`)
+)";
+
+try {
+    $connection->exec($sql);
+} catch (PDOException $e) {
+    echo 'Fehler beim Erstellen der Tabelle: ' . $e->getMessage();
+    exit();
+}
+?>
+
+<form action="save.php" method="post">
+    <label for="url">URL:</label>
+    <input type="url" name="url" required><br>
+
+    <label for="ziel">Ziel:</label>
+    <input type="ziel" name="ziel" required><br>
+
+    <input type="submit" value="Einfügen">
+</form>
+
+<?php
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/footer/acp.footer.inc.php");
+?>
