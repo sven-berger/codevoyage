1fa8749b2ee1eacf8a90dccfe5e0e3c9cfbec91b
sven-berger <bergersvenoliver@gmail.com>
2024-10-27 11:48:25 +0100

    sdf

--- acp/umsatzrechner/2023/index.php
+++ acp/umsatzrechner/2023/index.php
@@ -157,5 +157,5 @@ try {
     echo '<p style="text-align: center;">Es liegt ein Problem vor: ' . $e->getMessage() . '</p>';
 }
 
-    require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/footer/acp.footer.inc.php");
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/footer/acp.full.footer.inc.php");
 ?>
--- acp/umsatzrechner/2024/edit.php
+++ acp/umsatzrechner/2024/edit.php
@@ -64,5 +64,5 @@
         <?php endif; ?>
 
 <?php
-    require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/footer/acp.footer.inc.php");
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/footer/acp.full.footer.inc.php");
 ?>
