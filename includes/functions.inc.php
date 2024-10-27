090c5bb2ef6df2b63389fd29dbc50160c73d11aa
sven-berger <bergersvenoliver@gmail.com>
2024-10-27 10:37:39 +0100

    asd

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -4,22 +4,24 @@ require_once ($_SERVER['DOCUMENT_ROOT'] . "/includes/database.inc.php");
 try {
     $eigene_werke = "SELECT * FROM `php_sidebar_left_eigene_werke`";
     $spielereien = "SELECT * FROM `php_sidebar_left_spielereien`";
+    $sonstiges = "SELECT * FROM `php_sidebar_left_sonstiges`";
+
     $acp_sidebar_left_seitenleiste = "SELECT * FROM `acp_sidebar_left_seitenleiste`";
     $acp_sidebar_left_eigene_werke = "SELECT * FROM `acp_sidebar_left_eigene_werke`";
-    $acp_sidebar_left_sonstiges = "SELECT * FROM `acp_sidebar_left_sonstiges`";
 
     $ausgabe_eigene_werke = $connection->query($eigene_werke);
     $ausgabe_spielereien = $connection->query($spielereien);
-    
+    $ausgabe_sonstiges = $connection->query($sonstiges);
+
     $ausgabe_acp_sidebar_left_seitenleiste = $connection->query($acp_sidebar_left_seitenleiste);
     $ausgabe_acp_sidebar_left_eigene_werke = $connection->query($acp_sidebar_left_eigene_werke);
-    $ausgabe_acp_sidebar_left_sonstiges = $connection->query($acp_sidebar_left_sonstiges);
 
     $eigene_werke_liste = $ausgabe_eigene_werke->fetchAll(PDO::FETCH_ASSOC);
     $spielereien_liste = $ausgabe_spielereien->fetchAll(PDO::FETCH_ASSOC);
+    $sonstiges_liste = $ausgabe_spielereien->fetchAll(PDO::FETCH_ASSOC);
+
     $acp_sidebar_left_seitenleiste_liste = $ausgabe_acp_sidebar_left_seitenleiste->fetchAll(PDO::FETCH_ASSOC);
     $acp_sidebar_left_eigene_werke_liste = $ausgabe_acp_sidebar_left_eigene_werke->fetchAll(PDO::FETCH_ASSOC);
-    $acp_sidebar_left_sonstiges_liste = $ausgabe_acp_sidebar_left_sonstiges->fetchAll(PDO::FETCH_ASSOC);
 
 
 } catch (PDOException $e) {
