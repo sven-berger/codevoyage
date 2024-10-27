16afc2080fd9721d94738c2da31333a2c932789e
sven-berger <bergersvenoliver@gmail.com>
2024-10-27 11:44:29 +0100

    Update add.php

--- acp/sidebar/left/php/spielereien/add.php
+++ acp/sidebar/left/php/spielereien/add.php
@@ -1,7 +1,7 @@
 <?php
     $bereich = 'Administrationsbereich';
     $pageTitle = "Spielerei hinzufügen (PHP-Bereich)";
-    require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/header/app.header.inc.php");
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/header/core.header.inc.php");
 
 $sql = "
 CREATE TABLE IF NOT EXISTS `php_sidebar_left_spielereien`
@@ -31,5 +31,5 @@ try {
 </form>
 
 <?php
-    require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/footer/acp.footer.inc.php");
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/footer/acp.full.footer.inc.php");
 ?>
