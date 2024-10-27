5cadf78f60606281b24cecaf80a80302c0206940
sven-berger <bergersvenoliver@gmail.com>
2024-10-27 17:08:34 +0100

    asd

--- wissensportal/index.php
+++ wissensportal/index.php
@@ -1,8 +1,35 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://codevoyage.de/python/static/css/style.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.0/styles/default.min.css">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.0/highlight.min.js"></script>
+    <script>hljs.highlightAll();</script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <title><?php echo $pageTitle; ?></title>
+</head>
+
 <?php
-    $bereich = 'Wissensportal';
-    $pageTitle = 'Wissensportal';
-    require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/header/instance.app.header.inc.php");
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/../includes/database.inc.php");
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/../includes/functions.inc.php");
+    $mariadbVersion = getMariaDBVersion($connection);
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/../includes/var.inc.php");
 ?>
+
+<body>
+
+    <div class="header">
+    <a href="https://codevoyage.de/">
+        <h2 align="center">Willkommen auf meinem Apache-Webserver!</h2>
+    </a>
+    <h3 align="center">Dieser Server verwendet <a href="https://mariadb.org/" style="color: darkred;" target="_blank">MariaDB <?php echo htmlspecialchars($mariadbVersion); ?></a></h3>
+    <?php require_once ($_SERVER['DOCUMENT_ROOT'] . "/../includes/navigation.inc.php"); ?>
+    </div>
+    <div class="main">
+        <div class="content">
+            <h2><?php echo $pageTitle; ?></h2>
     
 <?php if (isset($_GET['snippet'])): ?>
 
@@ -64,9 +91,20 @@
 
 <?php else: ?>
     <? require_once ($_SERVER['DOCUMENT_ROOT'] . "/../lib/wissensportal.index.lib.php"); ?>
-
 <?php endif; ?>
 
-<?php    
-    require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/footer/wissensportal.footer.inc.php");
+</div>
+
+<?php
+    echo $wissensportal_sidebarLeft;
+    echo $wissensportal_sidebarRight;
 ?>
+
+</div>
+
+<?php
+    echo $footer;
+?>
+
+</body>
+</html>
