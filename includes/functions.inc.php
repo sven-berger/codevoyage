2970c2bcacf4806362ed6af94f4961e877359ca5
sven-berger <bergersvenoliver@gmail.com>
2024-10-27 12:23:42 +0100

    cbvb

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -8,6 +8,7 @@ try {
 
     $acp_sidebar_left_seitenleiste = "SELECT * FROM `acp_sidebar_left_seitenleiste`";
     $acp_sidebar_left_eigene_werke = "SELECT * FROM `acp_sidebar_left_eigene_werke`";
+    $acp_sidebar_left_eigene_werke = "SELECT * FROM `acp_sidebar_left_wissensportal`";
 
     $ausgabe_eigene_werke = $connection->query($eigene_werke);
     $ausgabe_spielereien = $connection->query($spielereien);
@@ -15,6 +16,7 @@ try {
 
     $ausgabe_acp_sidebar_left_seitenleiste = $connection->query($acp_sidebar_left_seitenleiste);
     $ausgabe_acp_sidebar_left_eigene_werke = $connection->query($acp_sidebar_left_eigene_werke);
+    $ausgabe_acp_sidebar_left_wissensportal = $connection->query($acp_sidebar_left_wissensportal);
 
     $eigene_werke_liste = $ausgabe_eigene_werke->fetchAll(PDO::FETCH_ASSOC);
     $spielereien_liste = $ausgabe_spielereien->fetchAll(PDO::FETCH_ASSOC);
@@ -22,7 +24,7 @@ try {
 
     $acp_sidebar_left_seitenleiste_liste = $ausgabe_acp_sidebar_left_seitenleiste->fetchAll(PDO::FETCH_ASSOC);
     $acp_sidebar_left_eigene_werke_liste = $ausgabe_acp_sidebar_left_eigene_werke->fetchAll(PDO::FETCH_ASSOC);
-
+    $acp_sidebar_left_wissensportal_liste = $ausgabe_acp_sidebar_left_wissensportal->fetchAll(PDO::FETCH_ASSOC);
 
 } catch (PDOException $e) {
     echo "<p style='color:red;'>Fehler bei der Abfrage: " . htmlspecialchars($e->getMessage()) . "</p>";
--- layout/sidebar/acp.sidebarLeft.php
+++ layout/sidebar/acp.sidebarLeft.php
@@ -22,6 +22,23 @@
 </section>
 
 <div class="boxCapital" style="margin-top: 20px;">
+    <p>Wissensportal</p>
+</div>
+<section class="sidebarBox">
+    <div class="boxContent">
+        <ul>
+            <?php if (!empty($acp_sidebar_left_wissensportal_liste)): ?>
+                <?php foreach ($acp_sidebar_left_wissensportal as $row): ?>
+                <li><a href="<?php echo htmlspecialchars($row['url']); ?>"><?php echo htmlspecialchars($row['ziel']); ?></a></li>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <li>Keine Snippets gefunden.</li>
+            <?php endif; ?>
+        </ul>
+    </div>
+</section>
+
+<div class="boxCapital" style="margin-top: 20px;">
     <p>Eigene Werke</p>
 </div>
 <section class="sidebarBox">
@@ -46,6 +63,7 @@
             </ul>
         </div>
 </section>
+
 <section class="sidebarBox">
         <h3 class="boxTitle">Blog</h3>
         <div class="boxContent">
