d4be8f3dd1f654fa2b799123a52a394aeef8d0c3
sven-berger <bergersvenoliver@gmail.com>
2024-10-27 17:49:36 +0100

    dfg

---
+++ acp/wissensportal/kategorien/index.php
@@ -0,0 +1,38 @@
+<?php
+    $bereich = 'Administrationsbereich';
+    $pageTitle = 'Wissensportal';
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/header/core.header.inc.php");
+    try {
+        $sql = "SELECT * FROM wissensportal_kategorien ORDER BY id DESC";
+        $stmt = $connection->query($sql);
+        $wissensportal_kategorien = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        echo "Fehler beim Laden der Snippets: " . htmlspecialchars($e->getMessage());
+        exit;
+    }
+?>
+
+<table>
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Aktionen</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($wissensportal_kategorien as $kategorie): ?>
+            <tr>
+                <td><?php echo htmlspecialchars($kategorie['name']); ?></td>
+                <td>
+                    <a href="edit.php?id=<?php echo $kategorie['id']; ?>">Bearbeiten</a> |
+                    <a href="delete.php?id=<?php echo $kategorie['id']; ?>" onclick="return confirm('Sicher, dass du dieses Snippet löschen willst?');">Löschen</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
+
+<?php
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/footer/acp.full.footer.inc.php");
+?>
---
+++ acp/wissensportal/kategorien/save.php
@@ -0,0 +1,29 @@
+<?php
+
+$bereich = 'Administrationsbereich';
+$pageTitle = 'Snippet abschicken';
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/header/core.header.inc.php");
+
+$sql = "
+CREATE TABLE IF NOT EXISTS `wissensportal_kategorien`
+(
+`ID` INT NOT NULL AUTO_INCREMENT,
+`ziel` VARCHAR(255) NOT NULL
+PRIMARY KEY (`ID`)
+)";
+
+try {
+    $connection->exec($sql);
+} catch (PDOException $e) {
+    echo 'Fehler beim Erstellen der Tabelle: ' . $e->getMessage();
+    exit();
+}
+
+$name = $_POST['name'];
+$sql = "INSERT INTO wissensportal_kategorie (name) VALUES (:name)";
+$stmt = $connection->prepare($sql);
+$stmt->execute([':name' => $name]);
+header("Location: https://codevoyage.de/acp/wissensportal/kategorien/index.php");
+
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/footer/acp.full.footer.inc.php");
+?>
