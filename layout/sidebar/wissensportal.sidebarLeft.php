83ba7b26474f40337212146e5c302827b15cae7b
sven-berger <bergersvenoliver@gmail.com>
2024-10-28 11:57:11 +0100

    ghj

--- layout/sidebar/wissensportal.sidebarLeft.php
+++ layout/sidebar/wissensportal.sidebarLeft.php
@@ -156,40 +156,6 @@
         </ul>
     </div>
 </section>
-
-<div class="boxCapital">
-    <p>Vorlagen</p>
-</div>
-<section class="sidebarBox">
-    <div class="boxContent">
-        <ul>
-        <?php if (empty($vorlagen_snippets)): ?>
-            <li>Kein Snippet vorhanden</li>
-        <?php else: ?>
-            <?php foreach ($vorlagen_snippets as $snippet): ?>
-            <li><a href="<?php echo 'index.php?snippet=' . htmlspecialchars($snippet['url']); ?>"><?php echo htmlspecialchars($snippet['title']); ?></a></li>
-            <?php endforeach; ?>
-        <?php endif; ?>
-        </ul>
-    </div>
-</section>
-
-<div class="boxCapital">
-    <p>Sonstiges</p>
-</div>
-<section class="sidebarBox">
-    <div class="boxContent">
-        <ul>
-        <?php if (empty($sonstiges_snippets)): ?>
-            <li>Kein Snippet vorhanden</li>
-        <?php else: ?>
-            <?php foreach ($sonstiges_snippets as $snippet): ?>
-            <li><a href="<?php echo 'index.php?snippet=' . htmlspecialchars($snippet['url']); ?>"><?php echo htmlspecialchars($snippet['title']); ?></a></li>
-            <?php endforeach; ?>
-        <?php endif; ?>
-        </ul>
-    </div>
-</section>
 </nav>
 
 
--- layout/sidebar/wissensportal.sidebarRight.php
+++ layout/sidebar/wissensportal.sidebarRight.php
@@ -1,32 +1,39 @@
+<?php
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/includes/functions.inc.php");
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/includes/database.inc.php");
+?>
+
 <aside>
-<div class="boxCapital" style="margin-top: 20px;">
+<div class="boxCapital">
     <p>Vorlagen</p>
 </div>
 <section class="sidebarBox">
     <div class="boxContent">
         <ul>
-            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=umsatzrechner">Umsatzrechner</a></li>
-            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=eintrittspreise">Eintrittspreis-Rechner</a></li>
-            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=passwortgenerator">Passwortgenerator</a></li>
-            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=einfaches-gewinnspiel">Einfaches Gewinnspiel</a></li>
-            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=mini-taschenrechner">Mini-Taschenrechner</a></li>
-            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=zahlen-raten">Zahlen raten</a></li>
-            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=speicherdaten-umrechner">Speicherdaten-Umrechner</a></li>
+        <?php if (empty($vorlagen_snippets)): ?>
+            <li>Kein Snippet vorhanden</li>
+        <?php else: ?>
+            <?php foreach ($vorlagen_snippets as $snippet): ?>
+            <li><a href="<?php echo 'index.php?snippet=' . htmlspecialchars($snippet['url']); ?>"><?php echo htmlspecialchars($snippet['title']); ?></a></li>
+            <?php endforeach; ?>
+        <?php endif; ?>
         </ul>
     </div>
 </section>
 
-<div class="boxCapital" style="margin-top: 20px;">
+<div class="boxCapital">
     <p>Sonstiges</p>
 </div>
 <section class="sidebarBox">
     <div class="boxContent">
         <ul>
-            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=screensaver">Screensaver</a></li>
-            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=schwarzes-loch">Ein schwartes Loch</a></li>
-            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=mathematische-operatoren-teil1">Mathematische Operatoren: Teil 1</a></li>
-            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=mathematische-operatoren-teil2">Mathematische Operatoren: Teil 2</a></li>
-            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=mathematische-operatoren-teil3">Mathematische Operatoren: Teil 3</a></li>
+        <?php if (empty($sonstiges_snippets)): ?>
+            <li>Kein Snippet vorhanden</li>
+        <?php else: ?>
+            <?php foreach ($sonstiges_snippets as $snippet): ?>
+            <li><a href="<?php echo 'index.php?snippet=' . htmlspecialchars($snippet['url']); ?>"><?php echo htmlspecialchars($snippet['title']); ?></a></li>
+            <?php endforeach; ?>
+        <?php endif; ?>
         </ul>
     </div>
 </section>
