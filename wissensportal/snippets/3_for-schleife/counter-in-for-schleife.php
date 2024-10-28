f085873ed83ba0ccbd3f6daec76f9399f37bf9d6
sven-berger <bergersvenoliver@gmail.com>
2024-10-28 14:32:56 +0100

    asd

--- wissensportal/snippets/3_for-schleife/counter-in-for-schleife.php
+++ wissensportal/snippets/3_for-schleife/counter-in-for-schleife.php
@@ -2,9 +2,13 @@
 $tiere = ["Wal", "Gans", "Affe", "Nashorn", "Zebra", "Esel"];
 $counter = 0;
 
+// foreach ($tiere as $tier) {
+//    $counter += 1;
+//    echo "$counter " . "ist " . $tier . "\n";
+// }
+
 foreach ($tiere as $tier) {
     $counter += 1;
     echo "Nr. $counter " . "ist " . $tier . "\n";
 }
-
 ?>
