52f48daf2a544aebc8a11b5dc52c81c82b0ba51e
sven-berger <bergersvenoliver@gmail.com>
2024-10-28 08:50:46 +0100

    sdf

--- acp/sidebar/left/acp/eigene-werke/save.php
+++ acp/sidebar/left/acp/eigene-werke/save.php
@@ -18,7 +18,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $prepare->execute();
 
             echo 'Menüpunkt erfolgreich eingetragen.';
-            header("Location: https://codevoyage.de/acp/sidebar/left/php/eigene-werke/add.php");
+            header("Location: https://codevoyage.de/acp/sidebar/left/acp/index.php");
             exit();
         } else {
             echo 'Bitte füllen Sie alle Felder aus.';
