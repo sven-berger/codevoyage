348687f1c48f67594d9e9ebf663ad883c15986c7
sven-berger <bergersvenoliver@gmail.com>
2024-10-28 11:27:45 +0100

    Update index.php

--- wissensportal/index.php
+++ wissensportal/index.php
@@ -1,7 +1,7 @@
 <?php
     $bereich = 'Wissensportal';
     $pageTitle = 'Wissensportal';
-    require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/header/instance.app.header.inc.php");
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/header/wissensportal.header.inc.php");
 ?>
     
 <?php if (isset($_GET['snippet'])): ?>
