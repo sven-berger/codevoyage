577fdc0950ca81579aa448079ab91d3417f9a2d6
sven-berger <bergersvenoliver@gmail.com>
2024-10-28 15:21:28 +0100

    Update index.php

--- wissensportal/index.php
+++ wissensportal/index.php
@@ -38,7 +38,7 @@
 
 <?php if (!empty($phpSnippet)): ?>
     <h3 class="section-title">PHP</h3>
-    <pre><code class="language-php"><?php echo htmlspecialchars_decode('<?php' . "\n" . $phpSnippet . "\n" . '?>'); ?></code></pre>
+    <pre><code class="language-php"><?php echo htmlspecialchars('<?php' . "\n" . $phpSnippet . "\n" . '?>'); ?></code></pre>
 <?php endif; ?>
 
 <?php if (!empty($php_snippet_alternativ)): ?>
