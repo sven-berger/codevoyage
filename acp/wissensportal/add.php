abaa27333e9108e26dfd95d688649459aab710ba
sven-berger <bergersvenoliver@gmail.com>
2024-10-28 11:11:55 +0100

    gj

--- acp/wissensportal/add.php
+++ acp/wissensportal/add.php
@@ -1,32 +1,40 @@
 <?php
     $bereich = 'Administrationsbereich';
     $pageTitle = 'Snippet hinzufügen';
-    require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/header/app.header.inc.php");
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/header/core.header.inc.php");
 ?>
 
 <form action="save.php" method="post">
-
+    <label for="kategorie">Kategorie:</label>
+    <select name="kategorie_id" id="kategorie" class="wissensportal-kategorien" required>
+    <?php
+    $kategorien = $connection->query("SELECT id, name FROM wissensportal_kategorien")->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($kategorien as $kategorie) {
+        echo "<option value='{$kategorie['id']}'>" . htmlspecialchars($kategorie['name']) . "</option>";
+    }
+    ?>
+    </select>
 
     <label for="title">Titel:</label>
-    <input type="text" name="title" required><br>
+    <input type="text" name="title" required>
 
     <label for="url">URL:</label>
-    <input type="text" name="url" required><br>
+    <input type="text" name="url" required>
 
     <label for="description">Beschreibung:</label>
-    <textarea name="description"></textarea><br>
+    <textarea name="description"></textarea>
 
     <label for="php_snippet">PHP Snippet:</label>
-    <textarea name="php_snippet"></textarea><br>
+    <textarea name="php_snippet"></textarea>
 
     <label for="php_snippet">PHP Snippet (Alternative Syntax):</label>
-    <textarea name="php_snippet_alternativ"></textarea><br>
+    <textarea name="php_snippet_alternativ"></textarea>
 
     <label for="python_snippet">Python Snippet:</label>
-    <textarea name="python_snippet"></textarea><br>
+    <textarea name="python_snippet"></textarea>
 
     <label for="javascript_snippet">JavaScript Snippet:</label>
-    <textarea name="javascript_snippet"></textarea><br>
+    <textarea name="javascript_snippet"></textarea>
 
     <label for="mitteilung_snippet">Mitteilung:</label>
     <textarea name="mitteilung_snippet"></textarea>
@@ -35,5 +43,5 @@
 </form>
 
 <?php 
-    require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/footer/acp.full.footer.inc.php");
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/footer/acp.footer.inc.php");
 ?>
