164ad76fce666c7939e9329176bc62fe5491a0f1
sven-berger <bergersvenoliver@gmail.com>
2024-10-28 11:16:50 +0100

    Update add.php

--- acp/wissensportal/add.php
+++ acp/wissensportal/add.php
@@ -16,25 +16,25 @@ require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/header/core.header.inc.php");
     </select>
 
     <label for="title">Titel:</label>
-    <input type="text" name="title" required>
+    <input type="text" name="title" required><br>
 
     <label for="url">URL:</label>
-    <input type="text" name="url" required>
+    <input type="text" name="url" required><br>
 
     <label for="description">Beschreibung:</label>
-    <textarea name="description"></textarea>
+    <textarea name="description"></textarea><br>
 
     <label for="php_snippet">PHP Snippet:</label>
-    <textarea name="php_snippet"></textarea>
+    <textarea name="php_snippet"></textarea><br>
 
     <label for="php_snippet">PHP Snippet (Alternative Syntax):</label>
-    <textarea name="php_snippet_alternativ"></textarea>
+    <textarea name="php_snippet_alternativ"></textarea><br>
 
     <label for="python_snippet">Python Snippet:</label>
-    <textarea name="python_snippet"></textarea>
+    <textarea name="python_snippet"></textarea><br>
 
     <label for="javascript_snippet">JavaScript Snippet:</label>
-    <textarea name="javascript_snippet"></textarea>
+    <textarea name="javascript_snippet"></textarea><br>
 
     <label for="mitteilung_snippet">Mitteilung:</label>
     <textarea name="mitteilung_snippet"></textarea>
