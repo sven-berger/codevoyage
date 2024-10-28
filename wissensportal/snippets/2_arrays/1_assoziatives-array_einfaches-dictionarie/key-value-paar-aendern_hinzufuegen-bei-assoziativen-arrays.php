ed5bc7223574c3dad6025daa556be6d28c505080
sven-berger <bergersvenoliver@gmail.com>
2024-10-28 13:23:08 +0100

    Update key-value-paar-aendern_hinzufuegen-bei-assoziativen-arrays.php

--- wissensportal/snippets/2_arrays/1_assoziatives-array_einfaches-dictionarie/key-value-paar-aendern_hinzufuegen-bei-assoziativen-arrays.php
+++ wissensportal/snippets/2_arrays/1_assoziatives-array_einfaches-dictionarie/key-value-paar-aendern_hinzufuegen-bei-assoziativen-arrays.php
@@ -13,13 +13,18 @@ $meine_person["Beruf"] = "Hans-Franz Chance";
 
 #Key-Value Pair hinzfügen
 $meine_person["Haarfarbe"] = "Dunkelblond";
-
 $meine_person["Katze"] = 
 [
     "Name" => "Anton",
     "Spitzname" => "Fettsack",
     "Beruf" => "Arbeitslos"
 ];
+
+#Einzelene Eigenschaft meiner Katze ausgeben
+print_r($meine_person["Katze"]["Spitzname"]);
+
+# Bestimmte Informationen im ganzen Satz ausgeben
+print_r("Der Name meiner Katze ist {meine_person['Katze']['Name']} und er ist ein {meine_person['Katze']['Beruf']}er Schmarotzer!");
 
 print_r($meine_person);
 
