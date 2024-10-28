ce1bef981077116e863a9f4d825227e60b515fd4
sven-berger <bergersvenoliver@gmail.com>
2024-10-28 13:05:32 +0100

    sdf

--- acp/wissensportal/add.php
+++ acp/wissensportal/add.php
@@ -1,7 +1,7 @@
 <?php
 $bereich = 'Administrationsbereich';
 $pageTitle = 'Snippet hinzufügen';
-require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/header/core.header.inc.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/header/app.header.inc.php");
 ?>
 
 <form action="" method="post">
@@ -78,5 +78,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 
-    require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/footer/acp.full.footer.inc.php");
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/footer/wissensportal.footer.inc.php");
 ?>
