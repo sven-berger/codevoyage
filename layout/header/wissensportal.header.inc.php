b25233d48e25808ea80d27b69cc88764047a529a
sven-berger <bergersvenoliver@gmail.com>
2024-10-28 11:31:23 +0100

    Update wissensportal.header.inc.php

--- layout/header/wissensportal.header.inc.php
+++ layout/header/wissensportal.header.inc.php
@@ -15,6 +15,7 @@
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/../includes/database.inc.php");
     $mariadbVersion = getMariaDBVersion($connection);
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/../includes/functions.inc.php");
+    
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/../includes/var.inc.php");
 ?>
 
