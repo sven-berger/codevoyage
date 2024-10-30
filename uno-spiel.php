a4ec5ba2e2417bc2c5619c72b0512914a3cf31b9
sven-berger <bergersvenoliver@gmail.com>
2024-10-30 12:59:20 +0100

    Update uno-spiel.php

--- uno-spiel.php
+++ uno-spiel.php
@@ -99,7 +99,7 @@ $spielkarten = $_SESSION['spielkarten'];
     ?>
 
     <!-- Überprüfen, ob die Karte gelegt werden kann -->
-    <?php if ($spielkarten[0]['farbe'] === $gewaehlte_farbe || $gewaehlte_karte === 'Farbwahl'): // Falls die Karte die gleiche Farbe hat oder es eine Farbwahl-Karte ist ?>
+    <?php if ($spielkarten[0]['farbe'] === $gewaehlte_farbe || $gewaehlte_karte === 'Farbwahl'): ?>
         <!-- Karte aus der Hand entfernen -->
         <?php foreach ($meine_karten as $key => $karte): ?>
             <?php if ($karte['name'] === $gewaehlte_karte && $karte['farbe'] === $gewaehlte_farbe): ?>
@@ -107,14 +107,24 @@ $spielkarten = $_SESSION['spielkarten'];
                 <?php break; ?>
             <?php endif; ?>
         <?php endforeach; ?>
-        
+
+        <!-- Aktualisiere die Handkarten in der Session -->
+        <?php $_SESSION['meine_karten'] = $meine_karten; ?>
+
         <!-- Erfolgreiche Nachricht -->
         <?php echo $section_beginn; ?>
             <p class="success" style="font-weight: bold; text-align: center">
                 Du hast folgende Karte gelegt: <?php echo $gewaehlte_karte . " (" . $gewaehlte_farbe . ")"; ?>
             </p>
         <?php echo $section_ende; ?>
-        <p></p>
+
+        <!-- Anzeige der verbleibenden Karten in der Hand -->
+        <h3>Verbleibende Karten:</h3>
+        <ul>
+            <?php foreach ($meine_karten as $karte): ?>
+                <li><?php echo $karte['name'] . " (" . $karte['farbe'] . ")"; ?></li>
+            <?php endforeach; ?>
+        </ul>
     <?php else: ?>
         <?php echo $section_beginn; ?>
             <p class="fail" style="font-weight: bold; text-align: center">Du kannst diese Karte nicht spielen, bitte wähle eine andere.</p>
