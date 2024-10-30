6d1eec73935edc3d3c750d6e72a18b68c4e0b44d
sven-berger <bergersvenoliver@gmail.com>
2024-10-30 10:17:57 +0100

    asd

---
+++ acp/sidebar/left/index.php
@@ -0,0 +1,45 @@
+<?php
+    $bereich = 'Administrationsbereich';
+    $pageTitle = "Linke Seitenleiste";
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/header/app.header.inc.php");
+?>
+
+<h3 class="section-title">Community-Spiele</h3>
+<section class="section">
+    <div class="sectionContent">
+        <?php try { ?>
+            <?php if (!empty($community_spiele_liste)) : ?>
+                <div class="ActionArea">
+                    <ul>
+                        <li><button class="button-action"><a href="https://codevoyage.de/acp/sidebar/left/community-spiele/add.php">Community-Spiel hinzufügen</a></button></li>
+                    </ul>
+                </div>     
+                <table>
+                    <tr>
+                        <th>URL</th>
+                        <th>Ziel</th>
+                        <th>Aktion</th>
+                    </tr>
+                    <?php foreach ($community_spiele_liste as $row) : ?>
+                        <tr>
+                            <td><?php echo htmlspecialchars($row['url']); ?></td>
+                            <td><?php echo htmlspecialchars($row['ziel']); ?></td>
+                            <td>
+                                <a href="https://codevoyage.de/acp/sidebar/left/community-spiele/edit.php?id=<?php echo htmlspecialchars($row['ID']); ?>">Bearbeiten</a> |
+                                <a href="https://codevoyage.de/acp/sidebar/left/community-spiele/delete.php?id=<?php echo htmlspecialchars($row['ID']); ?>" onclick="return confirm('Bist du dir sicher, dass du diesen Eintrag löschen möchtest?');">Löschen</a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </table>
+           <?php else : ?>
+                <p style="text-align: center;">Keine Einträge gefunden.</p>
+            <?php endif; ?>
+        <?php } catch (PDOException $e) { ?>
+            <p style="text-align: center;">Es liegt ein Problem vor: <?php echo htmlspecialchars($e->getMessage()); ?></p>
+        <?php } ?>
+    </div>
+</section>
+
+<?php
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/footer/acp.footer.inc.php");
+?>
--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -5,6 +5,7 @@ try {
     $eigene_werke = "SELECT * FROM `php_sidebar_left_eigene_werke`";
     $spielereien = "SELECT * FROM `php_sidebar_left_spielereien`";
     $sonstiges = "SELECT * FROM `php_sidebar_left_sonstiges`";
+    $community_spiele = "SELECT * FROM ´sidebar_left_community_spiele`";
 
     $acp_sidebar_left_seitenleiste = "SELECT * FROM `acp_sidebar_left_seitenleiste`";
     $acp_sidebar_left_eigene_werke = "SELECT * FROM `acp_sidebar_left_eigene_werke`";
@@ -14,6 +15,7 @@ try {
     $ausgabe_eigene_werke = $connection->query($eigene_werke);
     $ausgabe_spielereien = $connection->query($spielereien);
     $ausgabe_sonstiges = $connection->query($sonstiges);
+    $ausgabe_community_spiele = $connection->query($community_spiele);
 
     $ausgabe_acp_sidebar_left_seitenleiste = $connection->query($acp_sidebar_left_seitenleiste);
     $ausgabe_acp_sidebar_left_eigene_werke = $connection->query($acp_sidebar_left_eigene_werke);
@@ -23,6 +25,7 @@ try {
     $eigene_werke_liste = $ausgabe_eigene_werke->fetchAll(PDO::FETCH_ASSOC);
     $spielereien_liste = $ausgabe_spielereien->fetchAll(PDO::FETCH_ASSOC);
     $sonstiges_liste = $ausgabe_sonstiges->fetchAll(PDO::FETCH_ASSOC);
+    $community_spiele_liste = $ausgabe_community_spiele->fetchAll(PDO::FETCH_ASSOC);
 
     $acp_sidebar_left_seitenleiste_liste = $ausgabe_acp_sidebar_left_seitenleiste->fetchAll(PDO::FETCH_ASSOC);
     $acp_sidebar_left_eigene_werke_liste = $ausgabe_acp_sidebar_left_eigene_werke->fetchAll(PDO::FETCH_ASSOC);
