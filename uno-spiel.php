621ff8606723803f3a3334f8d16d1c804d9a4fa2
sven-berger <bergersvenoliver@gmail.com>
2024-10-30 11:23:49 +0100

    Update uno-spiel.php

--- uno-spiel.php
+++ uno-spiel.php
@@ -5,34 +5,51 @@
 ?>
 
 <?php
-
-$kartendeck = [
+$deck = [
     'Rot' => [
-        'Zahlen' => array_merge([0], array_fill(0, 2, range(1, 9))),
+        'Zahlen' => [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9],
         'Aktionen' => ['Zieh 2', 'Zieh 2', 'Richtungswechsel', 'Richtungswechsel', 'Aussetzen', 'Aussetzen']
     ],
     'Gelb' => [
-        'Zahlen' => array_merge([0], array_fill(0, 2, range(1, 9))),
+        'Zahlen' => [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9],
         'Aktionen' => ['Zieh 2', 'Zieh 2', 'Richtungswechsel', 'Richtungswechsel', 'Aussetzen', 'Aussetzen']
     ],
     'Grün' => [
-        'Zahlen' => array_merge([0], array_fill(0, 2, range(1, 9))),
+        'Zahlen' => [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9],
         'Aktionen' => ['Zieh 2', 'Zieh 2', 'Richtungswechsel', 'Richtungswechsel', 'Aussetzen', 'Aussetzen']
     ],
     'Blau' => [
-        'Zahlen' => array_merge([0], array_fill(0, 2, range(1, 9))),
+        'Zahlen' => [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9],
         'Aktionen' => ['Zieh 2', 'Zieh 2', 'Richtungswechsel', 'Richtungswechsel', 'Aussetzen', 'Aussetzen']
     ],
     'Spezial' => ['Farbwahl', 'Farbwahl', 'Farbwahl', 'Farbwahl', 'Farbwahl +4', 'Farbwahl +4', 'Farbwahl +4', 'Farbwahl +4']
 ];
 
+// Schritt 1: Deck in ein flaches Array umwandeln
+$flaches_deck = [];
+foreach ($deck as $farbe => $kartenarten) {
+    foreach ($kartenarten as $typ => $karten) {
+        foreach ($karten as $karte) {
+            $flaches_deck[] = $farbe . ' ' . $karte;
+        }
+    }
+}
+
+// Schritt 2: Deck mischen
+shuffle($flaches_deck);
+
+// Schritt 3: Karten verteilen
+$meine_karten = array_splice($flaches_deck, 0, 7);  // Die ersten 7 Karten für den Spieler
+$gegnerische_karten = array_splice($flaches_deck, 0, 7);  // Die nächsten 7 Karten für den Gegner
+
+// Ausgabe zum Testen
+echo "Meine Karten:\n";
+print_r($meine_karten);
+
+echo "\nGegnerische Karten:\n";
+print_r($gegnerische_karten);
 ?>
 
-<?php
-
-$meine_karten = [];
-$gegnerische_karten = [];
-?>
 
 
 <section class="section">
