511a15fafd525782ecd26dde398f4c21d7de61b2
sven-berger <bergersvenoliver@gmail.com>
2024-10-30 19:38:14 +0100

    Update uno-spiel.php

--- uno-spiel.php
+++ uno-spiel.php
@@ -22,7 +22,7 @@ $kartendeck = [
         'Zahlen' => [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9],
         'Aktionen' => ['Zieh 2', 'Zieh 2', 'Richtungswechsel', 'Richtungswechsel', 'Aussetzen', 'Aussetzen']
     ],
-    'Spezial' => ['Farbwahl', 'Farbwahl', 'Farbwahl', 'Farbwahl', 'Farbwahl +4', 'Farbwahl +4', 'Farbwahl +4', 'Farbwahl +4']
+    'Spezialkarten' => ['Farbwahl', 'Farbwahl', 'Farbwahl', 'Farbwahl', 'Farbwahl +4', 'Farbwahl +4', 'Farbwahl +4', 'Farbwahl +4']
 ];
 
 ?>
