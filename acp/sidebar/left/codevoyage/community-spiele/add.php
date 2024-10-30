62f27d4e087d1e439e6b6b17c9f675ef69948dec
sven-berger <bergersvenoliver@gmail.com>
2024-10-30 10:25:27 +0100

    klk

---
+++ acp/sidebar/left/codevoyage/community-spiele/add.php
@@ -0,0 +1,61 @@
+<?php
+    $bereich = 'Administrationsbereich';
+    $pageTitle = "Menüpunkt hinzufügen";
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/header/core.header.inc.php");
+
+$sql = "
+CREATE TABLE IF NOT EXISTS `sidebar_left_community_spiele`
+(
+`ID` INT NOT NULL AUTO_INCREMENT,
+`url` TEXT NOT NULL,
+`ziel` VARCHAR(255) NOT NULL,
+PRIMARY KEY (`ID`)
+)";
+
+try {
+    $connection->exec($sql);
+} catch (PDOException $e) {
+    echo 'Fehler beim Erstellen der Tabelle: ' . $e->getMessage();
+    exit();
+}
+?>
+
+<form action="" method="get">
+    <label for="url">URL:</label>
+    <input type="url" name="url" required><br>
+
+    <label for="ziel">Ziel:</label>
+    <input type="ziel" name="ziel" required><br>
+
+    <input type="submit" value="Einfügen">
+</form>
+
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    try {
+        if (!empty($_GET['url']) && !empty($_GET['ziel'])) {
+            $url = filter_input(INPUT_GET, 'url', FILTER_SANITIZE_SPECIAL_CHARS);
+            $ziel = filter_input(INPUT_GET, 'ziel', FILTER_SANITIZE_SPECIAL_CHARS);
+
+            $prepare = $connection->prepare('INSERT INTO `acp_sidebar_left_eigene_werke` (`url`, `ziel`) VALUES (:url, :ziel)');
+            $prepare->bindParam(':url', $url, PDO::PARAM_STR);
+            $prepare->bindParam(':ziel', $ziel, PDO::PARAM_STR);
+            $prepare->execute();
+
+            echo 'Menüpunkt erfolgreich eingetragen.';
+            header("Location: https://codevoyage.de/acp/sidebar/left/acp/index.php");
+            exit();
+        } else {
+            echo 'Bitte füllen Sie alle Felder aus.';
+        }
+    } catch (PDOException $e) {
+        echo 'Es liegt ein Problem vor: ' . htmlspecialchars($e->getMessage());
+    }
+}
+
+?>
+
+<?php
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/footer/acp.full.footer.inc.php");
+?>
