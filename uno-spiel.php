3df3bd7503aa1a9f039d48f2339a765d62f5867e
sven-berger <bergersvenoliver@gmail.com>
2024-10-30 18:59:44 +0100

    Update uno-spiel.php

--- uno-spiel.php
+++ uno-spiel.php
@@ -3,8 +3,6 @@ $bereich = 'Startseite';
 $pageTitle = 'UNO-Spiel';
 require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/header/app.header.inc.php");
 
-$section_beginn = "<section class='section'><div class='sectionContent'>";
-$section_ende = "</div><?php echo $section_ende; ?>";
 
 $kartendeck = [
     'Rot' => [
@@ -26,25 +24,7 @@ $kartendeck = [
     'Spezial' => ['Farbwahl', 'Farbwahl', 'Farbwahl', 'Farbwahl', 'Farbwahl +4', 'Farbwahl +4', 'Farbwahl +4', 'Farbwahl +4']
 ];
 
-// Array durchlaufen und Inhalte anzeigen
-foreach ($kartendeck as $farbe => $karten) {
-    echo "Farbe: $farbe\n";
-    
-    if (is_array($karten)) {
-        // Zeigt Zahlenkarten und Aktionskarten an
-        foreach ($karten as $typ => $werte) {
-            echo "  $typ: ";
-            echo implode(", ", $werte);
-            echo "\n";
-        }
-    } else {
-        // Zeigt Spezialkarten an
-        echo "  Spezialkarten: ";
-        echo implode(", ", $karten);
-        echo "\n";
-    }
-    echo "\n";
-}
+
 
 require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/footer/index.footer.inc.php");
 ?>
