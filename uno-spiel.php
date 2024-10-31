c26f06e08b2b763602beaff08df68db9a9a81f75
sven-berger <bergersvenoliver@gmail.com>
2024-10-31 16:57:06 +0100

    Update uno-spiel.php

--- uno-spiel.php
+++ uno-spiel.php
@@ -9,7 +9,12 @@ require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/header/app.header.inc.php");
     $gelbe_karten = ["0", "1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "Zieh 2", "Zieh 2", "Richtungswechsel", "Richtungswechsel", "Aussetzen", "Aussetzen"];
     $gruene_karten = ["0", "1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "Zieh 2", "Zieh 2", "Richtungswechsel", "Richtungswechsel", "Aussetzen", "Aussetzen"];
     $blaue_karten = ["0", "1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "Zieh 2", "Zieh 2", "Richtungswechsel", "Richtungswechsel", "Aussetzen", "Aussetzen"];
-    $spezialkarten = ["Farbwahl", "Farbwahl", "Farbwahl", "Farbwahl", "Farbwahl +4", "Farbwahl +4", "Farbwahl +4"]
+    $spezialkarten = ["Farbwahl", "Farbwahl", "Farbwahl", "Farbwahl", "Farbwahl +4", "Farbwahl +4", "Farbwahl +4"];
+
+    $spielkarten = array_merge($rote_karten, $gelbe_karten, $gruene_karten, $blaue_karten, $spielkarten);
+    shuffle($spielkarten);
+
+    print_r($spielkarten);
 ?>
 
 <?php echo $section_beginn; ?>
@@ -43,6 +48,19 @@ require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/header/app.header.inc.php");
         <li><?php echo $karte; ?></li>
     <?php endforeach; ?>
     </ul>
+<?php echo $section_ende; ?>
+
+<?php
+    $meine_karten = [];
+?>
+
+<div class="section-title">Meine Karten auf der Hand</div>
+<?php echo $section_beginn; ?>
+        <ul class="auflistung">
+            <?php foreach ($meine_karten as $meine_hand): ?>
+            <li><?php echo $meine_hand; ?></li>
+            <?php endforeach; ?>
+        </ul>
 <?php echo $section_ende; ?>
 
 <?php
