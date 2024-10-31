c8f13594982eed244a17140aa565bc7eaac66b7a
sven-berger <bergersvenoliver@gmail.com>
2024-10-31 16:33:11 +0100

    Update uno-spiel.php

--- uno-spiel.php
+++ uno-spiel.php
@@ -5,49 +5,22 @@ require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/header/app.header.inc.php");
 ?>
 
 <?php
-$kartendeck = [
-    'Rot' => [
-        'Zahlen' => [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9],
-        'Aktionen' => ['Zieh 2', 'Zieh 2', 'Richtungswechsel', 'Richtungswechsel', 'Aussetzen', 'Aussetzen']
-    ],
-    'Gelb' => [
-        'Zahlen' => [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9],
-        'Aktionen' => ['Zieh 2', 'Zieh 2', 'Richtungswechsel', 'Richtungswechsel', 'Aussetzen', 'Aussetzen']
-    ],
-    'Grün' => [
-        'Zahlen' => [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9],
-        'Aktionen' => ['Zieh 2', 'Zieh 2', 'Richtungswechsel', 'Richtungswechsel', 'Aussetzen', 'Aussetzen']
-    ],
-    'Blau' => [
-        'Zahlen' => [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9],
-        'Aktionen' => ['Zieh 2', 'Zieh 2', 'Richtungswechsel', 'Richtungswechsel', 'Aussetzen', 'Aussetzen']
-    ],
-    'Spezialkarten' => ['Farbwahl', 'Farbwahl', 'Farbwahl', 'Farbwahl', 'Farbwahl +4', 'Farbwahl +4', 'Farbwahl +4', 'Farbwahl +4']
-];
-
+    $rote_karten = ["0", "1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "Zieh 2", "Zieh 2", "Richtungswechsel", "Richtungswechsel", "Aussetzen", "Aussetzen"];
+    $gelbe_karten = ["0", "1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "Zieh 2", "Zieh 2", "Richtungswechsel", "Richtungswechsel", "Aussetzen", "Aussetzen"];
+    $gruene_karten = ["0", "1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "Zieh 2", "Zieh 2", "Richtungswechsel", "Richtungswechsel", "Aussetzen", "Aussetzen"];
+    $blaue_karten = ["0", "1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "Zieh 2", "Zieh 2", "Richtungswechsel", "Richtungswechsel", "Aussetzen", "Aussetzen"];
+    $spezialkarten = ["Farbwahl", "Farbwahl", "Farbwahl", "Farbwahl", "Farbwahl +4", "Farbwahl +4", "Farbwahl +4"]
 ?>
 
-<?php foreach ($kartendeck as $farbe => $wert): ?>
-    <h3 class="section-title"><?php echo $farbe; ?></h3>
-    <?php echo $section_beginn; ?>
-    <?php if (is_array($wert) && $farbe != 'Spezial'): ?>
-        <?php foreach ($wert as $typ => $werte): ?>
-            <div class="uno-typ"><?php echo $typ; ?></div>
-            <ul class="auflistung-uno">
-                <?php foreach ($werte as $einzelwert): ?>
-                    <li><?php echo $einzelwert; ?></li>
-                <?php endforeach; ?>
-            </ul>
-        <?php endforeach; ?>
-    <?php else: ?>
-        <ul class="auflistung-uno">
-            <?php foreach ($wert as $spezialkarte): ?>
-                <li><?php echo $spezialkarte; ?></li>
-            <?php endforeach; ?>
-        </ul>
-    <?php endif; ?>
+<h3 class="section-title">Rote Karten</h3>
+<?php foreach ($rote_karten AS $karte): ?>
+<?php echo $section_beginn; ?>
+    <ul class="auflistung-uno">
+        <li><?php echo $karte; ?></li>
+    </ul>
 <?php echo $section_ende; ?>
 <?php endforeach; ?>
+
 
 <style>
     .auflistung-uno {
