87ab28ecacff5002a22e077689742344bc84eac1
sven-berger <bergersvenoliver@gmail.com>
2024-11-04 18:50:19 +0100

    Update var.inc.php

--- includes/var.inc.php
+++ includes/var.inc.php
@@ -1,20 +1,21 @@
 <?php
 
-$header = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/layout/header.inc.php");
+$header = file_get_contents("/layout/header.inc.php");
 $footer = file_get_contents("https://codevoyage.de/python/templates/footer.html");
 
 $sidebarLeft = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/layout/sidebar/index.sidebarLeft.php");
 
-$wissensportal_sidebarLeft = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/layout/sidebar/wissensportal.sidebarLeft.php");
-$wissensportal_sidebarRight = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/layout/sidebar/wissensportal.sidebarLeft.php");
+$wissensportal_sidebarLeft = file_get_contents("/var/customers/webs/codevoyage/layout/sidebar/wissensportal.sidebarLeft.php");
+$wissensportal_sidebarRight = file_get_contents("/var/customers/webs/codevoyage/layout/sidebar/wissensportal.sidebarLeft.php");
 
-$acp_sidebarLeft = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/layout/sidebar/acp.sidebarLeft.php");
-$acp_sidebarRight = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/layout/sidebar/acp.sidebarLeft.php");
+$acp_sidebarLeft = file_get_contents("/var/customers/webs/codevoyage/layout/sidebar/acp.sidebarLeft.php");
+$acp_sidebarRight = file_get_contents("/var/customers/webs/codevoyage/layout/sidebar/acp.sidebarLeft.php");
 
-$php_sidebarLeft = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/layout/sidebar/php.sidebarLeft.php");
-$js_sidebarLeft = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/layout/sidebar/js.sidebarLeft.php");
+$php_sidebarLeft = file_get_contents("/var/customers/webs/codevoyage/layout/sidebar/php.sidebarLeft.php");
+$js_sidebarLeft = file_get_contents("/var/customers/webs/codevoyage/layout/sidebar/js.sidebarLeft.php");
 
-$sidebarLeft = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/layout/sidebar/index.sidebarLeft.php");
+$sidebarLeft = file_get_contents("/var/customers/webs/codevoyage/layout/sidebar/index.sidebarLeft.php");
+
 
 
 $section_beginn = "<section class='section'><div class='sectionContent'>";
