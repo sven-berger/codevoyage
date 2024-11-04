04fc03f502d92bcc0454494e86d38abc6606bdfc
sven-berger <bergersvenoliver@gmail.com>
2024-11-04 14:44:49 +0100

    asd

--- acp/sidebar/left/acp/einkaufsprozess/delete.php
+++ acp/sidebar/left/acp/einkaufsprozess/delete.php
@@ -24,7 +24,7 @@ try {
     $sql = "DELETE FROM acp_sidebar_left_einkaufsprozess WHERE id = :id";
     $stmt = $connection->prepare($sql);
     $stmt->execute([':id' => $id]);
-    header("Location: https://codevoyage.de/acp/sidebar/left/php/index.php");
+    header("Location: https://codevoyage.de/acp/sidebar/left/acp/index.php");
     exit;
 
 } catch (PDOException $e) {
