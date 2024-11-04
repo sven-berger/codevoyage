bff5f9d33d0aeb46686bc7d9238561bf8c631309
sven-berger <bergersvenoliver@gmail.com>
2024-11-04 16:54:03 +0100

    Update index.php

--- acp/navigation/index.php
+++ acp/navigation/index.php
@@ -1,7 +1,7 @@
 <?php
 $bereich = 'Administrationsbereich';
 $pageTitle = "Navigationspunkt hinzufügen";
-require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/header/core.header.inc.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/header/app.header.inc.php");
 
 $sql = "
 CREATE TABLE IF NOT EXISTS `navigation`
