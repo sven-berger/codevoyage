d2ce3b4fc339758c9021cd366ff5a9e3db9c8691
sven-berger <bergersvenoliver@gmail.com>
2024-11-04 18:22:03 +0100

    Update app.header.inc.php

--- layout/header/app.header.inc.php
+++ layout/header/app.header.inc.php
@@ -25,7 +25,6 @@
         <h2 align="center">Willkommen auf meinem Apache-Webserver!</h2>
     </a>
     <h3 align="center">Dieser Server verwendet <a href="https://mariadb.org/" style="color: darkred;" target="_blank">MariaDB <?php echo htmlspecialchars($mariadbVersion); ?></a></h3>
-    <?php require_once ($_SERVER['DOCUMENT_ROOT'] . "/includes/navigation.inc.php"); ?>
     </div>
     <div class="main">
         <div class="content">
