4c5e040f78062296005645d7bdc744ec7f26e475
sven-berger <bergersvenoliver@gmail.com>
2024-11-04 19:03:21 +0100

    Update var.inc.php

--- includes/var.inc.php
+++ includes/var.inc.php
@@ -1,16 +1,17 @@
 <?php
 
-$footer = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/python/templates/footer.html");
+$footer = file_get_contents("https://codevoyage.de/python/templates/footer.html");
 
-$sidebarLeft = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/layout/sidebar/index.sidebarLeft.php");
-$wissensportal_sidebarLeft = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/layout/sidebar/wissensportal.sidebarLeft.php");
-$wissensportal_sidebarRight = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/layout/sidebar/wissensportal.sidebarRight.php");
+$sidebarLeft = file_get_contents("https://codevoyage.de/layout/sidebar/index.sidebarLeft.php");
 
-$acp_sidebarLeft = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/layout/sidebar/acp.sidebarLeft.php");
-$acp_sidebarRight = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/layout/sidebar/acp.sidebarRight.php");
+$wissensportal_sidebarLeft = file_get_contents("https://codevoyage.de/layout/sidebar/wissensportal.sidebarLeft.php");
+$wissensportal_sidebarRight = file_get_contents("https://codevoyage.de/layout/sidebar/wissensportal.sidebarRight.php");
 
-$php_sidebarLeft = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/layout/sidebar/php.sidebarLeft.php");
-$js_sidebarLeft = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/layout/sidebar/js.sidebarLeft.php");
+$acp_sidebarLeft = file_get_contents("https://codevoyage.de/layout/sidebar/acp.sidebarLeft.php");
+$acp_sidebarRight = file_get_contents("https://codevoyage.de/layout/sidebar/acp.sidebarRight.php");
+
+$php_sidebarLeft = file_get_contents("https://codevoyage.de/layout/sidebar/php.sidebarLeft.php");
+$js_sidebarLeft = file_get_contents("https://codevoyage.de/layout/sidebar/js.sidebarLeft.php");
 
 $section_beginn = "<section class='section'><div class='sectionContent'>";
 $section_ende = "</div></section>";
