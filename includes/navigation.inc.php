f29f50bbcbd1d6c3c608561eaac168c404ab918a
sven-berger <bergersvenoliver@gmail.com>
2024-11-04 18:24:50 +0100

    klj

--- includes/navigation.inc.php
+++ includes/navigation.inc.php
@@ -1,12 +1,5 @@
 <?php
-try {
-    $navigation_header = "SELECT * FROM `navigation_header` ORDER BY `reihenfolge` ASC";
-    $ausgabe_navigation_header = $connection->query($navigation_header);
-    $navigation_header_liste = $ausgabe_navigation_header->fetchAll(PDO::FETCH_ASSOC);
-    if (!isset($page)) $page = '';
-} catch (PDOException $e) {
-    echo "<p style='color:red;'>Fehler bei der Abfrage: " . htmlspecialchars($e->getMessage()) . "</p>";
-}
+if (!isset($page)) $page = '';
 ?>
 
 <div class="navigation">
