e5e656152bc34fa2f63ac0aee441e2798896529b
sven-berger <bergersvenoliver@gmail.com>
2024-11-04 17:15:38 +0100

    Update index.php

--- acp/sidebar/left/acp/index.php
+++ acp/sidebar/left/acp/index.php
@@ -4,6 +4,42 @@
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/header/app.header.inc.php");
 ?>
 
+<h3 class="section-title">Navigation</h3>
+<section class="section">
+    <div class="sectionContent">
+    <div class="ActionArea">
+        <ul>
+            <li><button class="button-action"><a href="https://codevoyage.de/acp/sidebar/left/acp/navigation/add.php">Menüpunkt hinzufügen</a></button></li>
+        </ul>
+    </div>      
+        <?php try { ?>
+            <?php if (!empty($acp_sidebar_left_navigation_header_liste)) : ?>   
+                <table>
+                    <tr>
+                        <th>URL</th>
+                        <th>Ziel</th>
+                        <th>Aktion</th>
+                    </tr>
+                    <?php foreach ($acp_sidebar_left_navigation_header_liste as $row) : ?>
+                        <tr>
+                            <td><?php echo htmlspecialchars($row['url']); ?></td>
+                            <td><?php echo htmlspecialchars($row['ziel']); ?></td>
+                            <td>
+                                <a href="https://codevoyage.de/acp/sidebar/left/acp/navigation/edit.php?id=<?php echo htmlspecialchars($row['ID']); ?>">Bearbeiten</a> |
+                                <a href="https://codevoyage.de/acp/sidebar/left/acp/navigation/delete.php?id=<?php echo htmlspecialchars($row['ID']); ?>" onclick="return confirm('Bist du dir sicher, dass du diesen Eintrag löschen möchtest?');">Löschen</a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </table>
+           <?php else : ?>
+                <p style="text-align: center;">Keine Einträge gefunden.</p>
+            <?php endif; ?>
+        <?php } catch (PDOException $e) { ?>
+            <p style="text-align: center;">Es liegt ein Problem vor: <?php echo htmlspecialchars($e->getMessage()); ?></p>
+        <?php } ?>
+    </div>
+</section>
+
 <h3 class="section-title">Linke Seitenleiste</h3>
 <section class="section">
     <div class="sectionContent">
