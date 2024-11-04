bfab83dda35d1c328f55e31dc49fb0f6aaed2808
sven-berger <bergersvenoliver@gmail.com>
2024-11-04 18:53:10 +0100

    Update var.inc.php

--- includes/var.inc.php
+++ includes/var.inc.php
@@ -2,7 +2,7 @@
 
 $footer = file_get_contents("https://codevoyage.de/python/templates/footer.html");
 
-$sidebarLeft = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/layout/sidebar/index.sidebarLeft.php");
+$sidebarLeft = file_get_contents("/layout/sidebar/index.sidebarLeft.php");
 
 $wissensportal_sidebarLeft = file_get_contents("/var/customers/webs/codevoyage/layout/sidebar/wissensportal.sidebarLeft.php");
 $wissensportal_sidebarRight = file_get_contents("/var/customers/webs/codevoyage/layout/sidebar/wissensportal.sidebarLeft.php");
