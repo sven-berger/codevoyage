3ae6c891f532d93560fbdb893773aa05ee9d11e0
sven-berger <bergersvenoliver@gmail.com>
2024-11-04 16:52:21 +0100

    Update acp.sidebarLeft.php

--- layout/sidebar/acp.sidebarLeft.php
+++ layout/sidebar/acp.sidebarLeft.php
@@ -5,6 +5,23 @@
 <nav>
 
 <div class="boxCapital" style="margin-top: 20px;">
+    <p>Navigation</p>
+</div>
+<section class="sidebarBox">
+    <div class="boxContent">
+        <ul>
+            <?php if (!empty($navigation_liste)): ?>
+                <?php foreach ($anavigation_liste as $row): ?>
+                <li><a href="<?php echo htmlspecialchars($row['url']); ?>"><?php echo htmlspecialchars($row['ziel']); ?></a></li>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <li>Keine Einträge gefunden.</li>
+            <?php endif; ?>
+        </ul>
+    </div>
+</section>
+
+<div class="boxCapital" style="margin-top: 20px;">
     <p>Linke Seitenleiste</p>
 </div>
 <section class="sidebarBox">
