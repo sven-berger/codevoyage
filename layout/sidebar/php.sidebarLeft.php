b9be4f50918a4673ee9e58686f5737f1319f70d4
sven-berger <bergersvenoliver@gmail.com>
2024-11-04 19:06:58 +0100

    Update php.sidebarLeft.php

--- layout/sidebar/php.sidebarLeft.php
+++ layout/sidebar/php.sidebarLeft.php
@@ -1,5 +1,5 @@
 <?php
-    require_once "/var/customers/webs/codevoyage/includes/database.inc.php";
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/includes/instance.functions.inc.php");
 ?>
 
 <nav>
