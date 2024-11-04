b7ee7118f9650b8a4230d8e84651f03170c6f8e0
sven-berger <bergersvenoliver@gmail.com>
2024-11-04 18:37:51 +0100

    Update navigation.inc.php

--- includes/navigation.inc.php
+++ includes/navigation.inc.php
@@ -1,6 +1,6 @@
 <?php
 if (!isset($page)) $page = '';
-require_once ($_SERVER['DOCUMENT_ROOT'] . "/../includes/database.inc.php");
+require_once $_SERVER['DOCUMENT_ROOT'] . "/includes/database.inc.php";
 $navigation_header = "SELECT * FROM `navigation_header` ORDER BY `reihenfolge` ASC";
 $ausgabe_navigation_header = $connection->query($navigation_header);
 $navigation_header_liste = $ausgabe_navigation_header->fetchAll(PDO::FETCH_ASSOC);
