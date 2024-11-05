c001b5e9c0f9b9d6e4790bd93ea68bd2991dd846
sven-berger <bergersvenoliver@gmail.com>
2024-11-05 09:26:01 +0100

    Update wissensportal.sidebarLeft.php

--- layout/sidebar/wissensportal.sidebarLeft.php
+++ layout/sidebar/wissensportal.sidebarLeft.php
@@ -1,5 +1,5 @@
 <?php
-    require_once ($_SERVER['DOCUMENT_ROOT'] . "/includes/sql/wissensportal.sql.php");
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/../includes/sql/wissensportal.sql.php");
 ?>
 
 
