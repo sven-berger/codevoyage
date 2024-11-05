88e4bbccbc2c6e480e7f07c06b1a7bc534b2ac17
sven-berger <bergersvenoliver@gmail.com>
2024-11-05 10:34:08 +0100

    Update wissensportal.footer.inc.php

--- layout/footer/wissensportal.footer.inc.php
+++ layout/footer/wissensportal.footer.inc.php
@@ -1,18 +1,14 @@
 </div>
 
 <?php
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/../includes/sql/wissensportal.sql.php");
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/sidebar/wissensportal.sidebarLeft.php");
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/sidebar/wissensportal.sidebarRight.php");
+
     // $wissensportal_sidebarLeft = file_get_contents("https://codevoyage.de/layout/sidebar/wissensportal.sidebarLeft.php");
    //$wissensportal_sidebarRight = file_get_contents("https://codevoyage.de/layout/sidebar/wissensportal.sidebarRight.php");
    // echo $wissensportal_sidebarLeft; 
   //  echo $wissensportal_sidebarRight;
-?>
-
-<?php
-    require_once ($_SERVER['DOCUMENT_ROOT'] . "/../includes/sql/wissensportal.sql.php");
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
 ?>
 
 </div>
