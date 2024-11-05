8ab22ff62331131e49e82cf52f4c3772bc008387
sven-berger <bergersvenoliver@gmail.com>
2024-11-05 09:17:02 +0100

    dgfg

--- wissensportal/index.php
+++ wissensportal/index.php
@@ -1,7 +1,7 @@
 <?php
     $bereich = 'Wissensportal';
     $pageTitle = 'Wissensportal';
-    require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/header/wissensportal.header.inc.php");
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/header/instance.header.inc.php");
 ?>
     
 <?php if (isset($_GET['snippet'])): ?>
