0da2e043cc38adff059dd26a086ae51706a16489
sven-berger <bergersvenoliver@gmail.com>
2024-11-05 08:59:45 +0100

    cbvvn

--- includes/var.inc.php
+++ includes/var.inc.php
@@ -1,6 +1,5 @@
 <?php
 
-$footer = file_get_contents("https://codevoyage.de/python/templates/footer.html");
 
 $sidebarLeft = file_get_contents("https://codevoyage.de/layout/sidebar/index.sidebarLeft.php");
 
@@ -13,8 +12,5 @@ $acp_sidebarRight = file_get_contents("https://codevoyage.de/layout/sidebar/acp.
 
 
 $js_sidebarLeft = file_get_contents("https://codevoyage.de/layout/sidebar/js.sidebarLeft.php");
-
-$section_beginn = "<section class='section'><div class='sectionContent'>";
-$section_ende = "</div></section>";
 
 $test = "Hello World aus var.inc.php";
--- layout/footer/php.footer.inc.php
+++ layout/footer/php.footer.inc.php
@@ -7,7 +7,8 @@
 </div>
 
 <?php
-    echo $footer;
+   // $footer = file_get_contents("https://codevoyage.de/python/templates/footer.html");
+    require_once "/var/customers/webs/codevoyage/python/templates/footer.html";;
 ?>
 
 </body>
