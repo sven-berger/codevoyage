b1c48ec77b786946487032229861d25f63856b82
sven-berger <bergersvenoliver@gmail.com>
2024-11-05 10:35:55 +0100

    Update wissensportal.footer.inc.php

--- layout/footer/wissensportal.footer.inc.php
+++ layout/footer/wissensportal.footer.inc.php
@@ -3,11 +3,6 @@
 <?php
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/sidebar/wissensportal.sidebarLeft.php");
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/sidebar/wissensportal.sidebarRight.php");
-
-    // $wissensportal_sidebarLeft = file_get_contents("https://codevoyage.de/layout/sidebar/wissensportal.sidebarLeft.php");
-   //$wissensportal_sidebarRight = file_get_contents("https://codevoyage.de/layout/sidebar/wissensportal.sidebarRight.php");
-   // echo $wissensportal_sidebarLeft; 
-  //  echo $wissensportal_sidebarRight;
 ?>
 
 </div>
