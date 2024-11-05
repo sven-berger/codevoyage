e36c231f17cb77d5fc020d69efaa3b764caa64c4
sven-berger <bergersvenoliver@gmail.com>
2024-11-05 09:01:04 +0100

    Update php.footer.inc.php

--- layout/footer/php.footer.inc.php
+++ layout/footer/php.footer.inc.php
@@ -1,14 +1,14 @@
 </div>
 
 <?php
-    require_once "/var/customers/webs/codevoyage/layout/sidebar/php.sidebarLeft.php";;
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/sidebar/php.sidebarLeft.php");
 ?>
 
 </div>
 
 <?php
    // $footer = file_get_contents("https://codevoyage.de/python/templates/footer.html");
-    require_once "/var/customers/webs/codevoyage/python/templates/footer.html";;
+    require_once "/var/customers/webs/codevoyage/python/templates/footer.html";
 ?>
 
 </body>
