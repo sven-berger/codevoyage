2eeb87e2e161c513991e2af544a772757e049010
sven-berger <bergersvenoliver@gmail.com>
2024-11-05 10:33:02 +0100

    ewr

--- layout/sidebar/wissensportal.sidebarLeft.php
+++ layout/sidebar/wissensportal.sidebarLeft.php
@@ -1,8 +1,5 @@
 <?php
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/../includes/sql/wissensportal.sql.php");
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
 ?>
 
 <nav>
