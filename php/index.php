2767f0b0bfd942d12389d51863fb18bb3aaa4451
sven-berger <bergersvenoliver@gmail.com>
2024-11-05 08:56:40 +0100

    bvn

--- php/index.php
+++ php/index.php
@@ -1,7 +1,7 @@
 <?php
     $bereich = 'PHP-Bereich';
     $pageTitle = 'Startseite der PHP-Instanz';
-    require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/header/instance.core.header.inc.php");
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/header/header.inc.php");
 ?>
 
 <?php
@@ -9,5 +9,5 @@ echo 'Hallo und herzlich willkommen im PHP-Bereich!';
 ?>
 
 <?php
-    require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/footer/php.full.footer.inc.php");
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/footer/php.footer.inc.php");
 ?>
