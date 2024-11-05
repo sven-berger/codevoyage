b4703fdb36ed606193b7b9db9db3b6cf26f0a739
sven-berger <bergersvenoliver@gmail.com>
2024-11-05 11:27:04 +0100

    Update index.footer.inc.php

--- layout/footer/index.footer.inc.php
+++ layout/footer/index.footer.inc.php
@@ -1,11 +1,14 @@
 </div>
+
 <?php
-    echo $sidebarLeft;
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/sidebar/index.sidebarLeft.php");
 ?>
 
 </div>
 
+
 <?php
+    $footer = file_get_contents("https://codevoyage.de/python/templates/footer.html");
     echo $footer;
 ?>
 
