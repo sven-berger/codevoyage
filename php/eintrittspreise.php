a1baef33f076b3714e42abfa290ecb17198216b6
sven-berger <bergersvenoliver@gmail.com>
2024-11-11 16:07:34 +0100

    Update eintrittspreise.php

--- php/eintrittspreise.php
+++ php/eintrittspreise.php
@@ -15,7 +15,6 @@ try {
 ?>
 
 <?php if (count($ausgabe) > 0): ?>
-<?php echo $section_beginn; ?>
     <table class="table-eintrittspreise">
             <tr>
                 <th>Alter</th>
@@ -30,7 +29,6 @@ try {
     </table>
 <?php else: ?>
     <p>Keine Eintrittspreise gefunden.</p>
-<?php echo $section_ende; ?>
 <?php endif; ?>
 
 <?php if (!isset($_GET['alter'])): ?>
