4ccad2d85ac70646eef0578a407a77b7c1f42b24
sven-berger <bergersvenoliver@gmail.com>
2024-11-11 15:08:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/header/header.inc.php");
 ?>
 
+<?php echo $section_beginn; ?>
 <?php
     echo "Hallo und herzlich willkommen auf CodeVoyage.de"
 ?>
+<?php echo $section_ende; ?>
 
 <?php
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/footer/index.footer.inc.php");
