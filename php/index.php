a315f9929e7901e14d38cd557bdbea83c9962c7c
sven-berger <bergersvenoliver@gmail.com>
2024-11-12 09:40:53 +0100

    Update index.php

--- php/index.php
+++ php/index.php
@@ -4,9 +4,11 @@
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/header/instance.header.inc.php");
 ?>
 
-<?php
+<?php echo $section_beginn; ?>
+<?php 
 echo 'Hallo und herzlich willkommen im PHP-Bereich!';
 ?>
+<?php echo $section_ende; ?>
 
 <?php
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/footer/php.footer.inc.php");
