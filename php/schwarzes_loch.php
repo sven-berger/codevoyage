0a68116028e95b594ae612b427a991e8b34541d3
sven-berger <bergersvenoliver@gmail.com>
2024-11-12 09:21:03 +0100

    Update schwarzes_loch.php

--- php/schwarzes_loch.php
+++ php/schwarzes_loch.php
@@ -4,9 +4,56 @@
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/header/instance.header.inc.php");
 ?>
 
-<style>#black-hole{background:#000;display:grid;place-content:center;height:100vh;filter:blur(8px)}black-hole,black-hole:after,black-hole:before{display:block;border-radius:50%}black-hole{width:50vmin;height:51vmin;box-shadow:1vmin 0 3vmin 2vmin #f50,inset -1vmin 0 3vmin 4vmin #f80,-4vmin 0 35vmin 0 #f60;animation:bh 5s linear infinite}@keyframes bh{0%{transform:rotate(0)}100%{transform:rotate(360deg)}}black-hole:after,black-hole:before{content:"";background:#fff;position:relative;top:6vmin}black-hole:before{width:3vmin;height:3vmin;left:40vmin;box-shadow:#ff0 0 0 2vmin 2vmin,#ff0 2vmin 4vmin 2vmin .5vmin}black-hole:after{width:38vmin;height:38vmin;opacity:.03;left:-14vmin}</style>
+<div id="black-hole"></div>
 
-<div id="black-hole"><black-hole /></div>
+<style>
+#black-hole {
+    background:#000;
+    display:grid;
+    place-content:center;
+    height:75vh;
+    filter:blur(8px)
+}
+
+black-hole, black-hole:after, black-hole:before {
+    display:block;
+    border-radius:50%
+}
+
+black-hole {
+    width:50vmin;
+    height:51vmin;
+    box-shadow:1vmin 0 3vmin 2vmin #f50,inset -1vmin 0 3vmin 4vmin #f80,-4vmin 0 35vmin 0 #f60;
+    animation:bh 5s linear infinite;
+}
+
+@keyframes bh {
+    0%{transform:rotate(0)}100%{transform:rotate(360deg)}
+}
+    
+black-hole:after, black-hole:before {
+    content:"";
+    background:#fff;
+    position:relative;
+    top:6vmin;
+}
+
+black-hole:before {
+    width:3vmin;
+    height:3vmin;
+    left:40vmin;
+    box-shadow:#ff0 0 0 2vmin 2vmin, #ff0 2vmin 4vmin 2vmin .5vmin;
+}
+
+black-hole:after {
+    width:38vmin;
+    height:38vmin;
+    opacity:.03;
+    left:-14vmin;
+}
+</style>
+
+
 
 <?php
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/../layout/footer/php.footer.inc.php");
