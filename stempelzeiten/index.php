ea38b6474d8b397ef6a198724269ee8604fbe5ae
sven-berger <bergersvenoliver@gmail.com>
2024-11-12 09:51:29 +0100

    Update index.php

--- stempelzeiten/index.php
+++ stempelzeiten/index.php
@@ -7,11 +7,14 @@ require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/header/header.inc.php");
 <?php echo $section_beginn; ?>
 <form action="" method="post">
     <label for="eingeloggt">Eingeloggt:</label>
-    <input type="datetime-local" name="eingeloggt" required><br>
+    <input type="datetime-local" name="eingeloggt" 
+           value="<?php echo date('d.m.Y') . 'T00:00'; ?>" required><br>
 
     <label for="ausgeloggt">Ausgeloggt:</label>
-    <input type="datetime-local" name="ausgeloggt" required><br>
+    <input type="datetime-local" name="ausgeloggt" 
+           value="<?php echo date('d.m.Y') . 'T00:00'; ?>" required><br>
 
+    <label for="ort">Ort:</label>
     <select name="ort" id="ort">
         <option value="Standort">Standort</option>
         <option value="Homeoffice">Homeoffice</option>
@@ -20,6 +23,7 @@ require_once ($_SERVER['DOCUMENT_ROOT'] . "/layout/header/header.inc.php");
     <button type="submit">Hinzufügen</button>
     <button type="reset">Zurücksetzen</button>
 </form>
+
 <?php echo $section_ende; ?>
 
 
