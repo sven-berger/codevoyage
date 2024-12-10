3c56edde6942c123dc6407ae7c35211d6fe782c4
Sven Oliver Berger <bergersvenoliver@gmail.com>
2024-12-10 15:35:22 +0100

    Update index.sidebarLeft.php

--- layout/sidebar/index.sidebarLeft.php
+++ layout/sidebar/index.sidebarLeft.php
@@ -16,17 +16,17 @@
         </div>
     </section>
     <div class="boxCapital" style="margin-top: 20px;">
-        <p>Community-Spiele</p>
+        <p>Drittanbieter von WoltLab</p>
     </div>
     <section class="sidebarBox">
         <div class="boxContent">
             <ul>
-                <?php if (!empty($community_spiele_liste)): ?>
-                    <?php foreach ($community_spiele_liste as $row): ?>
+                <?php if (!empty($drittanbieter_woltlab_liste)): ?>
+                    <?php foreach ($drittanbieter_woltlab_liste as $row): ?>
                     <li><a href="<?php echo htmlspecialchars($row['url']); ?>"><?php echo htmlspecialchars($row['ziel']); ?></a></li>
                     <?php endforeach; ?>
                 <?php else: ?>
-                    <li>Keine Community-Spiele gefunden.</li>
+                    <li>Keine Drittanbieter gefunden.</li>
                 <?php endif; ?>
             </ul>
         </div>
