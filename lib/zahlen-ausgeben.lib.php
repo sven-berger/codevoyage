f6867cdf475cd1452ac24a95ab6060b2fefd6b6a
Sven Oliver Berger <bergersvenoliver@gmail.com>
2025-03-30 18:18:23 +0200

    Schönheitskorrekturen

--- lib/zahlen-ausgeben.lib.php
+++ lib/zahlen-ausgeben.lib.php
@@ -1,17 +1,17 @@
-    <form method="POST" action="">
-        <label for="beginnerzahl">Beginnerzahl</label>
-        <input type="number" id="beginnerzahl" name="beginnerzahl" required>
+<form method="POST" action="">
+    <label for="beginnerzahl">Beginnerzahl</label>
+    <input type="number" id="beginnerzahl" name="beginnerzahl" required>
         
-        <label for="endzahl">Endzahl</label>
-        <input type="number" id="endzahl" name="endzahl" required>
+    <label for="endzahl">Endzahl</label>
+    <input type="number" id="endzahl" name="endzahl" required>
 
-        <label for="schrittweise">Schrtittweise</label>
-        <input type="number" id="schrittweise" name="schrittweise">
+    <label for="schrittweise">Schrtittweise</label>
+    <input type="number" id="schrittweise" name="schrittweise">
 
-        <button type="submit">Abschicken</button>
-    </form>
+    <button type="submit">Abschicken</button>
+</form>
 
-    <?php if (isset($_POST['beginnerzahl']) && isset($_POST['endzahl'])): ?>
+<?php if (isset($_POST['beginnerzahl']) && isset($_POST['endzahl'])): ?>
     <?php
         $beginnerzahl = (int)$_POST['beginnerzahl'];
         $schrittweise = isset($_POST['schrittweise']) && $_POST['schrittweise'] > 0 ? (int)$_POST['schrittweise'] : 1;
