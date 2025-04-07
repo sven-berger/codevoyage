ad95ab5d44243a2397f87b9ade87ec794ecc8b0e
Sven Oliver Berger <bergersvenoliver@gmail.com>
2025-04-07 09:31:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
-<?php require_once ($_SERVER['DOCUMENT_ROOT'] . "/includes/header.php");
-
-if (headers_sent($file, $line)) {
-    echo "Headers already sent in $file on line $line";
-    exit;
-}
-?>
+<?php require_once ($_SERVER['DOCUMENT_ROOT'] . "/includes/header.php"); ?>
 
 <?php
-
     // Standardseite setzen
     $page = $_GET['page'] ?? '';
 
