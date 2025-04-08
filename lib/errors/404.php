85d713d2cd96481119bb573039546b1b6d2026b2
Sven Oliver Berger <bergersvenoliver@gmail.com>
2025-04-08 13:06:00 +0200

    asd

--- lib/errors/404.php
+++ lib/errors/404.php
@@ -1,2 +1,4 @@
-<h2>404 - Seite nicht gefunden</h2>
-<a href="index.php">Zurück zur Startseite</a>
+<div class="bg-white round rounded-3 border p-3">
+    <h4>404 - Seite nicht gefunden</h4>
+    <a href="index.php">Zurück zur Startseite</a>
+</div>
--- lib/pages/index.page.php
+++ lib/pages/index.page.php
@@ -1,2 +1,4 @@
-<p>Hallo</p>
-<p>Dies ist die Startseite der Webseite.</p>
+<div class="bg-white round rounded-3 border p-3">
+    <p>Hallo</p>
+    <p>Dies ist die Startseite der Webseite.</p>
+</div>
