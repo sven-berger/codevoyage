9c88a81be05f97910de82ea98e9aad1b06f2c785
Sven Oliver Berger <bergersvenoliver@gmail.com>
2025-04-23 17:55:05 +0200

    Fix "Typo"

--- 3s1d/edit.php
+++ 3s1d/edit.php
@@ -113,7 +113,7 @@
     <script src="../assets/tinymce/tinymce.min.js"></script>
     <script>
     tinymce.init({
-        license_key: 'gpl'
+        license_key: 'gpl',
         selector: 'textarea',
         plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
         toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
